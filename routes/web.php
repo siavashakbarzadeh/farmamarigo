28a1f122e5ac49617ea43890fe13c75a9392ee82
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-11 16:26:47 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Botble\Ecommerce\Jobs\OrderSubmittedJob;
+use Botble\Ecommerce\Mail\OrderConfirmed;
+use Illuminate\Support\Facades\Mail;
+
+\Illuminate\Support\Facades\Route::get('/mail',function (){
+    Mail::to($this->order->address->email)->send(new OrderConfirmed($this->order));
+    OrderSubmittedJob::dispatch(\Botble\Ecommerce\Models\Order::query()->latest()->first());
+});
+
+Route::get('/importDbfrom',function(){
+    return view('import');
+});
