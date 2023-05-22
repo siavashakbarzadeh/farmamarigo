798f077e047393076fbd93d624b8ba9650f76adf
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-22 14:39:46 +0200

    Update footer.blade.php

--- platform/themes/wowy/partials/footer.blade.php
+++ platform/themes/wowy/partials/footer.blade.php
@@ -110,20 +110,10 @@
     //
     // });
     $('.brands-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+            items : 4,
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [979,3]
     })
     $('.featured-brands-carousel').owlCarousel({
         autoplay:true,
