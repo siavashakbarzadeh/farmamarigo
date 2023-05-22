2507fb321fd926943ac50516c0664783612a1501
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-22 12:54:11 +0200

    Update footer.blade.php

--- platform/themes/wowy/partials/footer.blade.php
+++ platform/themes/wowy/partials/footer.blade.php
@@ -59,6 +59,37 @@
             "No reviews!": "{{ __('No reviews!') }}"
         };
     </script>
+    <script>
+        $('.featured-brands-carousel').owlCarousel({
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
+            stagePadding: 50,/*the little visible images at the end of the carousel*/
+            loop:true,
+            rtl: false,
+            lazyLoad:true,
+            autoHeight:true,
+            margin:-60,
+            nav:false,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:2
+                },
+                800:{
+                    items: 3
+                },
+                1000:{
+                    items:4
+                },
+                1200:{
+                    items: 4
+                }
+            }
+        })
+    </script>
 
     {!! Theme::place('footer') !!}
 
