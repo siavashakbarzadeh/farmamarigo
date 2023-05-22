1d3995be3905e3ed77d784cea60d8fe3177b8e04
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-22 12:58:56 +0200

    Update WowyController.php

--- platform/themes/wowy/src/Http/Controllers/WowyController.php
+++ platform/themes/wowy/src/Http/Controllers/WowyController.php
@@ -108,7 +108,7 @@ class WowyController extends PublicController
      * @param BaseHttpResponse $response
      * @return BaseHttpResponse
      */
-    public function ajaxGetFeaturedBrands(Request $request, BaseHttpResponse $response)
+    public static function ajaxGetFeaturedBrands(Request $request, BaseHttpResponse $response)
     {
         if (! $request->ajax() || ! $request->wantsJson()) {
             return $response->setNextUrl(route('public.index'));
