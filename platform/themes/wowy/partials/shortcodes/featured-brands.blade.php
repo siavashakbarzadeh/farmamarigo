0d94ba72cbad8bb6fa6088125489c641cb014bfe
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-22 14:50:31 +0200

    1451

--- platform/themes/wowy/partials/shortcodes/featured-brands.blade.php
+++ platform/themes/wowy/partials/shortcodes/featured-brands.blade.php
@@ -6,7 +6,7 @@
         <h3 class="section-title style-1 mb-30 wow fadeIn animated">{!! BaseHelper::clean($title) !!}</h3>
 {{--        <div id="owl-demo" class="owl-carousel owl-theme">--}}
         <div class=" owl-carousel owl-theme featured-brands-carousel ">
-        <div class=" owl-carousel owl-theme brands-carousel ">
+{{--        <div class=" owl-carousel owl-theme brands-carousel ">--}}
 
             @foreach ($brands as $brand)
                 <div class="col-6">
