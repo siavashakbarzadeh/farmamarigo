219ed4b68a50c4d11bc4b3864afda38c621bd62a
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-26 18:27:34 +0200

    wd

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -8,7 +8,7 @@
     $products=DB::connection('mysql2')->table("art_articolo")->whereIn('categoria',[6,15,17])->whereIn('fk_linea_id',[443,441,439,383,295,124])->get();
 
     foreach ($products as $product) {
-
+        dd($product->pk_articolo_id);
         $productItem = \Botble\Ecommerce\Models\Product::updateOrCreate([
 //        $productItem=DB::connection('mysql')->table('ec_products')->updateOrInsert([
 
