e851aebf14c3913d3bd4129162f7ce4b55515245
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-26 18:33:36 +0200

    Update Product.php

--- platform/plugins/ecommerce/src/Models/Product.php
+++ platform/plugins/ecommerce/src/Models/Product.php
@@ -28,6 +28,9 @@ class Product extends BaseModel
 {
     protected $table = 'ec_products';
 
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+
     protected $fillable = [
         'name',
         'description',
