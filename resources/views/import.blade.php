f52997e5a516f0ee93903c75c0b44d51ad8e1222
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-26 18:48:44 +0200

    wwww

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -6,6 +6,7 @@
 //    $products=DB::connection('mysql2')->select('SELECT * FROM `art_articolo` WHERE categoria=6 OR categoria=15 OR categoria=17;');
 
     $products=DB::connection('mysql2')->table("art_articolo")->whereIn('categoria',[6,15,17])->whereIn('fk_linea_id',[443,441,439,383,295,124])->get();
+    dd(count($products));
 
     try {
         \Illuminate\Support\Facades\DB::transaction(function ()use($products){
