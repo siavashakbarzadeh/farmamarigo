034820a35e13a7feb44561178daaa32c18990e2a
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-26 19:32:22 +0200

    aa

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -6,7 +6,7 @@
 //    $products=DB::connection('mysql2')->select('SELECT * FROM `art_articolo` WHERE categoria=6 OR categoria=15 OR categoria=17;');
 
     $products=DB::connection('mysql2')->table("art_articolo")->whereIn('categoria',[6,15,17])->whereIn('fk_linea_id',[443,441,439,383,295,124])->get();
-    $items = \Botble\Ecommerce\Models\Product::query()->all()->pluck('name')->toArray();
+    $items = \Botble\Ecommerce\Models\Product::query()->get()->pluck('name')->toArray();
     try {
         $products = $products->map(function ($item){
             return (array)$item;
