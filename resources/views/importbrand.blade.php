4a61204bd172f2202ee3a9ead907151147a48899
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-29 13:15:50 +0200

    1315

--- resources/views/importbrand.blade.php
+++ resources/views/importbrand.blade.php
@@ -5,9 +5,9 @@
 
 //    $products=DB::connection('mysql2')->select('SELECT * FROM `art_articolo` WHERE categoria=6 OR categoria=15 OR categoria=17;');
 
-    $products=DB::connection('mysql2')->table("art_articolo")->where('categoria',[6,15,17])->get();
-//    $fornitore=$products->fk_fornitore_id;
-//     @dd($fornitore);
+    $products=DB::connection('mysql2')->table("art_articolo")->select('fk_fornitore_id')->where('categoria',[6,15,17])->get();
+
+     @dd($products);
 //    $products=DB::connection('mysql2')->table("art_articolo")->where('categoria',[6,15,17])->whereIn('fk_fornitore_id',[443,441,439,383,295,124])->get();
 
     $items = \Botble\Ecommerce\Models\Product::query()->get()->pluck('name')->toArray();
