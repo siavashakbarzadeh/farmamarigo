b808cb592458143bde8994d54c0b6ee26e071b7a
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-30 11:36:06 +0200

    ww

--- resources/views/importbrand.blade.php
+++ resources/views/importbrand.blade.php
@@ -6,6 +6,9 @@
 //    $products=DB::connection('mysql2')->select('SELECT * FROM `art_articolo` WHERE categoria=6 OR categoria=15 OR categoria=17;');
 
     $brandsId=DB::connection('mysql2')->table("art_articolo")->select('fk_fornitore_id')->where('categoria',[6,15,17])->get();
+    dd($brandsId,collect($brandsId)->map(function ($item){
+        return (array)$item;
+    }));
     $brandsId = collect($brandsId)->map(function ($item){
         return (array)$item;
     })->pluck('fk_fornitore_id')->unique();
@@ -15,7 +18,7 @@
 
           $cbrand = \Botble\Ecommerce\Models\Brand::updateOrCreate([
                         'name' => $brand->nome,
-                       
+
 
                     ]);
      }
