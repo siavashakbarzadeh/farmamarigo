8e4fce99b90558a764cb8e734e021eb16c76c5de
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-30 11:13:54 +0200

    Update import.blade.php

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -5,7 +5,7 @@
 
 //    $products=DB::connection('mysql2')->select('SELECT * FROM `art_articolo` WHERE categoria=6 OR categoria=15 OR categoria=17;');
 
-dd(class_basename(new \Botble\Ecommerce\Models\Product()));
+dd((new \Botble\Ecommerce\Models\Product())->getMorphClass());
     $products=DB::connection('mysql2')->table("art_articolo")->whereIn('categoria',[6,15,17])->whereIn('fk_linea_id',[443,441,439,383,295,124])->get();
     $items = \Botble\Ecommerce\Models\Product::query()->get()->pluck('name')->toArray();
     $products = $products->map(function ($item){
