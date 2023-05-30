0b9cc941bcd91d6da7439afa654766baef6f02fd
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-30 11:33:54 +0200

    Update importbrand.blade.php

--- resources/views/importbrand.blade.php
+++ resources/views/importbrand.blade.php
@@ -6,9 +6,9 @@
 //    $products=DB::connection('mysql2')->select('SELECT * FROM `art_articolo` WHERE categoria=6 OR categoria=15 OR categoria=17;');
 
     $brandsId=DB::connection('mysql2')->table("art_articolo")->select('fk_fornitore_id')->where('categoria',[6,15,17])->get();
-    dd(collect($brandsId)->map(function ($item){
+    $brandsId = collect($brandsId)->map(function ($item){
         return (array)$item;
-    })->pluck('fk_fornitore_id')->unique());
+    })->pluck('fk_fornitore_id')->unique();
     $brands=DB::connection('mysql2')->table("acq_fornitore")->whereIn('pk_fornitore_id',$brandsId->toArray())->get();
     @dd($brands);
      foreach ($brands as $brand){
