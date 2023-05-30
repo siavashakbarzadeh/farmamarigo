0710f064e21cfb5597d73e9b20a149cb8e05e4df
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-30 12:31:21 +0200

    1231

--- resources/views/importbrand.blade.php
+++ resources/views/importbrand.blade.php
@@ -6,7 +6,7 @@
 //    $products=DB::connection('mysql2')->select('SELECT * FROM `art_articolo` WHERE categoria=6 OR categoria=15 OR categoria=17;');
 
     $brandsId=DB::connection('mysql2')->table("art_articolo")->select('fk_fornitore_id')->where('categoria',[6,15,17])->get();
-  
+
     $brandsId = collect($brandsId)->map(function ($item){
         return (array)$item;
     })->pluck('fk_fornitore_id')->unique();
---
+++ resources/views/importtaxes.blade.php
@@ -0,0 +1,26 @@
+@php
+
+    $taxes=DB::connection('mysql2')->select('select * from arc_codice_iva');
+           $taxes_updated=array();
+           foreach ($taxes as $tax) {
+             $row=DB::connection('mysql')->table('ec_taxes')->updateOrInsert(
+               [
+                   'id' => $tax->pk_codice_iva_id,
+                   'nome' => $tax->nome,
+               ]
+               ,
+               [
+               'codice'=>$tax->codice,
+               'desc'=>$tax->descrizione,
+               'percentage'=>$tax->percentuale,
+               'tipo'=>$tax->tipo,
+               'status' => 'published',
+               ]
+           );
+           if($row==true) array_push($taxes_updated, $row);
+           }
+           if(empty($taxes_updated)) return 'No tax record updated';
+           else return $taxes_updated;
+
+
+@endphp
