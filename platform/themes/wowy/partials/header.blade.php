5d740c8f96c7021045d9c361b40b754f81f0096b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-31 14:35:39 +0200

    1435

--- platform/themes/wowy/partials/header.blade.php
+++ platform/themes/wowy/partials/header.blade.php
@@ -130,19 +130,19 @@
 
 
                                                                 <div class="row" style="width: 100%">
-                                                                    <a class="bg-primary text-light item-profile-wrap" href="https://dev.marigo.collaudo.biz/customer/logout">Esci</a>
+                                                                    <a class="bg-primary text-light item-profile-wrap" href="https://marigopharma.marigo.collaudo.biz/customer/logout">Esci</a>
                                                                 </div>
                                                                 <div class="row" style="width: 100%">
-                                                                    <a class="bg-light item-profile-wrap" href="https://dev.marigo.collaudo.biz/customer/edit-account">Profilo</a>
+                                                                    <a class="bg-light item-profile-wrap" href="https://marigopharma.marigo.collaudo.biz//customer/edit-account">Profilo</a>
                                                                 </div>
                                                                 <div class="row" style="width: 100%">
-                                                                    <a class="bg-light item-profile-wrap" href="https://dev.marigo.collaudo.biz/customer/orders">Ordini</a>
+                                                                    <a class="bg-light item-profile-wrap" href="https://marigopharma.marigo.collaudo.biz/customer/orders">Ordini</a>
                                                                 </div>
                                                                 <div class="row" style="width: 100%">
-                                                                    <a class="bg-light item-profile-wrap" href="https://dev.marigo.collaudo.biz/wishlist">Desideri</a>
+                                                                    <a class="bg-light item-profile-wrap" href="https://marigopharma.marigo.collaudo.biz/wishlist">Desideri</a>
                                                                 </div>
                                                                 <div class="row" style="width: 100%">
-                                                                    <a class="bg-light item-profile-wrap" href="https://dev.marigo.collaudo.biz/customer/change-password">Password</a>
+                                                                    <a class="bg-light item-profile-wrap" href="https://marigopharma.marigo.collaudo.biz/change-password">Password</a>
                                                                 </div>
                                                                 <div class="row" style="width: 100%">
                                                                     <a class="bg-light item-profile-wrap" style="margin-bottom:0 !important" href="https://dev.marigo.collaudo.biz/customer/overview">IL TUO ACCOUNT</a>
