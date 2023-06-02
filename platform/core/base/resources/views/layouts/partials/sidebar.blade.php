b73dc5c9052432a3291431e73ecaf7663257e951
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-02 19:57:19 +0200

    0757

--- platform/core/base/resources/views/layouts/partials/sidebar.blade.php
+++ platform/core/base/resources/views/layouts/partials/sidebar.blade.php
@@ -4,7 +4,7 @@
 @foreach ($menus as $menu)
     @php $menu = apply_filters(BASE_FILTER_DASHBOARD_MENU, $menu); @endphp
     @if ($menu['id']=='cms-plugins-ads' || $menu['id']=='cms-plugins-newsletter'
-|| $menu['id']=='cms-plugins-simple-slider' 
+|| $menu['id']=='cms-plugins-simple-slider'
 || $menu['id']=='cms-plugins-location'
     || $menu['id']=='cms-core-plugins' || $menu['id']=='cms-core-platform-administration' )
 
