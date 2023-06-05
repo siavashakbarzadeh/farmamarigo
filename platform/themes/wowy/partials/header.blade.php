7f48f78a53b6932794cf4701968ed9f7a89d8a13
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-05 11:38:59 +0200

    Update header.blade.php

--- platform/themes/wowy/partials/header.blade.php
+++ platform/themes/wowy/partials/header.blade.php
@@ -53,7 +53,7 @@
             <div class="header header-top-ptb-1 d-none d-lg-block">
                 <div class="container">
                     <div class="row align-items-center">
-                        <div class="col-xl-3 col-lg-8">
+                        <div class="col-xl-8 col-lg-8">
                             <div class="header-info">
                                 <ul>
 {{--                                    @if (theme_option('hotline'))--}}
