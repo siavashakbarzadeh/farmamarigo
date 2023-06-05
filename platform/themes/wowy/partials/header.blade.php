5955d082304acaea374bd4c04a877ab6e43468c7
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-05 11:30:00 +0200

    1129

--- platform/themes/wowy/partials/header.blade.php
+++ platform/themes/wowy/partials/header.blade.php
@@ -62,6 +62,12 @@
                                     @if (theme_option('phone'))
                                         <li><i class="fa fa-phone-alt mr-5"></i><a href="tel:{{ theme_option('phone') }}">{{ theme_option('phone') }}</a></li>
                                     @endif
+                                    @if (theme_option('email'))
+                                        <li><i class="fa fa-phone-alt mr-5"></i><a href="tel:{{ theme_option('email') }}">{{ theme_option('email') }}</a></li>
+                                    @endif
+                                    @if (theme_option('address'))
+                                        <li><i class="fa fa-phone-alt mr-5"></i><a href="tel:{{ theme_option('address') }}">{{ theme_option('address') }}</a></li>
+                                    @endif
 
                                     @if (is_plugin_active('ecommerce') && EcommerceHelper::isOrderTrackingEnabled())
                                         <li><i class="far fa-anchor mr-5"></i><a href="{{ route('public.orders.tracking') }}">{{ __('Track Your Order') }}</a></li>
