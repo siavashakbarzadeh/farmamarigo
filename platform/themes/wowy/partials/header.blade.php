ef168bb3f691e9a4b764652a3b0d4e98a47f149b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-05 11:57:09 +0200

    Update header.blade.php

--- platform/themes/wowy/partials/header.blade.php
+++ platform/themes/wowy/partials/header.blade.php
@@ -66,9 +66,9 @@
                                     @if (theme_option('phone'))
                                         <li><i class="fa fa-phone-alt mr-5"></i><a href="tel:{{ theme_option('phone') }}">{{ theme_option('phone') }}</a></li>
                                     @endif
-{{--                                    @if (theme_option('email'))--}}
-{{--                                        <li><i class="fa fa-phone-alt mr-5"></i><a href="tel:{{ theme_option('email') }}">{{ theme_option('email') }}</a></li>--}}
-{{--                                    @endif--}}
+                                    @if (theme_option('email'))
+                                        <li><i class="fa fa-phone-alt mr-5"></i><a href="tel:{{ theme_option('email') }}">{{ theme_option('email') }}</a></li>
+                                    @endif
 {{--                                    @if (theme_option('address'))--}}
 {{--                                        <li><i class="fa fa-phone-alt mr-5"></i><a href="tel:{{ theme_option('address') }}">{{ theme_option('address') }}</a></li>--}}
 {{--                                    @endif--}}
