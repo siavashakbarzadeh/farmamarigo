3670c0cabeaba5b3af9d9c5d86e906034edd54d1
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-06 13:41:11 +0200

    Update CustomImport.php

--- platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
+++ platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
@@ -264,6 +264,8 @@ class CustomImport extends BaseController
         });
         dd($products->filter(function ($item){
             return strlen($item['variante_1']);
+        })->groupBy(function ($item){
+            dd($item['variante_1']);
         }));
         $brandsId=DB::connection('mysql2')->table("art_articolo")->select('fk_fornitore_id')->where('fk_fornitore_id',$products->pluck('fk_fornitore_id')->toArray())->get();
         $brandsId = collect($brandsId)->map(function ($item){
