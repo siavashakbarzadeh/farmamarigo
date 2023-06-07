7ed1f7b60bf1abfd9fd9ad6037638f187ab6ffe3
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-07 13:33:39 +0200

    Update verification-account.blade.php

--- resources/views/mails/verification-account.blade.php
+++ resources/views/mails/verification-account.blade.php
@@ -4,7 +4,8 @@
     <title>Document</title>
 </head>
 <body>
-Vi preghiamo di confermare la vostra richiesta di iscrizione premendo il seguente link:
+<p>Vi preghiamo di confermare la vostra richiesta di iscrizione premendo il seguente link:</p>
+
 <a href="{{ $url }}" target="_blank">{{ $url }}</a>
 </body>
 </html>
