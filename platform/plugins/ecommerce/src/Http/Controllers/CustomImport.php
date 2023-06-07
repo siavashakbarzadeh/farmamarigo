b97a972ee7b81592a95348ccf03f2cf54e5aea73
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-07 11:45:58 +0200

    Update CustomImport.php

--- platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
+++ platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
@@ -218,6 +218,11 @@ class CustomImport extends BaseController
                             'reference_type' => $productItem->getMorphClass(),
                             'prefix' => "products"
                         ]);
+                        dd(collect($productItem->toArray())
+                            ->put('is_variation',1)
+                            ->put('quantity',null)
+                            ->put('cost_per_item',null)
+                            ->toArray());
                         if ($variationItems->count()) {
                             $productVariation = ProductVariation::create([
                                 'product_id' => Product::create(collect($productItem->toArray())
