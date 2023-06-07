c43aebad07c1bda09d5e1f97d4c0963b0a257e57
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-07 13:28:54 +0200

    1328

--- resources/views/mails/verification-account.blade.php
+++ resources/views/mails/verification-account.blade.php
@@ -4,6 +4,7 @@
     <title>Document</title>
 </head>
 <body>
+Vi preghiamo di confermare la vostra richiesta di iscrizione premendo il seguente link:
 <a href="{{ $url }}" target="_blank">{{ $url }}</a>
 </body>
 </html>
