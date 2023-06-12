11417d3959bf1358bf5408a182c4000a0d1028e8
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-12 12:37:42 +0200

    1237

--- platform/plugins/ecommerce/src/Http/Controllers/Customers/LoginController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Customers/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends Controller
             Cache::put($key,"generated",now()->addMinutes(5));
             $url = URL::signedRoute('customer.user-verify',['id'=>auth('customer')->user()->id],now()->addMinutes(5));
             Mail::to(auth('customer')->user()->email)->send(new VerificationAccountMail($url));
-            return redirect('users/verify');
+            return redirect('customer/edit-account');
         }
     }
 
