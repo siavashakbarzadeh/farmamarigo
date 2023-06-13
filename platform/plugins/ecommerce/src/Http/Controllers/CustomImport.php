f985c371a0d87250f097e40189ce421482c46836
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-13 09:29:30 +0200

    Update CustomImport.php

--- platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
+++ platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
@@ -237,7 +237,7 @@ class CustomImport extends BaseController
                         $this->_generateSlugProduct($product_name,$productItem);
                         if ($variationItems->count()) {
                             foreach ($variationItems as $variationItem) {
-                                dd($variationItems,$variationItem);
+                                dd($products,$variationItems);
                                 $productVariation = ProductVariation::create([
                                     'product_id' => Product::create([
                                         'name' => $productItem->name,
