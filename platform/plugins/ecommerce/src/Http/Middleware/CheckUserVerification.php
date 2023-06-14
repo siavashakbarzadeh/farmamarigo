02f9ee8a40e1a3bb1f0f29da1e438d60e99c3e2e
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-14 13:31:44 +0200

    Update CheckUserVerification.php

--- platform/plugins/ecommerce/src/Http/Middleware/CheckUserVerification.php
+++ platform/plugins/ecommerce/src/Http/Middleware/CheckUserVerification.php
@@ -16,9 +16,14 @@ class CheckUserVerification
 {
     public function handle(Request $request, Closure $next)
     {
-        $url = URL::signedRoute('customer.user-verify',['id'=>auth('customer')->user()->id],now()->addMinutes(5));
-        Mail::to(auth('customer')->user()->email)->send(new VerificationAccountMail($url));
-        dd("salma");
+        try {
+            $url = URL::signedRoute('customer.user-verify',['id'=>auth('customer')->user()->id],now()->addMinutes(5));
+            Mail::to(auth('customer')->user()->email)->send(new VerificationAccountMail($url));
+            dd("salma");
+        }catch (\Throwable $e){
+            dd($e);
+        }
+
 //        auth()->user()->markEmailAsVerified();
 //        auth()->user()->unMarkEmailAsVerified();
         if (auth('customer')->user() && !auth('customer')->user()->email_verified_at ) {
