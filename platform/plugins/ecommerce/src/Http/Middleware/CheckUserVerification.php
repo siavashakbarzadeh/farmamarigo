9c468d0b4d55c20019c2152f549c72979101f9c7
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-14 14:13:33 +0200

    Update CheckUserVerification.php

--- platform/plugins/ecommerce/src/Http/Middleware/CheckUserVerification.php
+++ platform/plugins/ecommerce/src/Http/Middleware/CheckUserVerification.php
@@ -16,7 +16,7 @@ class CheckUserVerification
 {
     public function handle(Request $request, Closure $next)
     {
-        dd("salam");
+        dd("salammmmmmmmmm");
 //        auth()->user()->markEmailAsVerified();
 //        auth()->user()->unMarkEmailAsVerified();
         if (auth('customer')->user() && !auth('customer')->user()->email_verified_at ) {
