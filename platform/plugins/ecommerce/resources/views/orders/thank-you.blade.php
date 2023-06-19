a552a1ac99181475c740a00a4886e978f5f9613b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-06-19 10:23:44 +0200

    Update thank-you.blade.php

--- platform/plugins/ecommerce/resources/views/orders/thank-you.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/thank-you.blade.php
@@ -6,7 +6,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-7 col-md-6 col-12 ">
+            <div class="col-lg-12 col-md-6  ">
                 @include('plugins/ecommerce::orders.partials.logo')
 
                 <div class="thank-you">
