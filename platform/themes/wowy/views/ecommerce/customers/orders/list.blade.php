a5dbb228c7307164b30000a638e3fe11e468e547
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-07-03 14:14:56 +0200

    Update list.blade.php

--- platform/themes/wowy/views/ecommerce/customers/orders/list.blade.php
+++ platform/themes/wowy/views/ecommerce/customers/orders/list.blade.php
@@ -22,9 +22,52 @@
                                 <td>{{ get_order_code($order->id) }}</td>
                                 <td>{{ $order->created_at->format('Y/m/d h:m') }}</td>
                                 <td>{{ __(':price for :total item(s)', ['price' => $order->amount_format, 'total' => $order->products_count]) }}</td>
-                                <td>{{ $order->status->label() }}</td>
                                 <td>
-                                    <a class="btn-small d-block" href="{{ route('customer.orders.view', $order->id) }}">{{ __('View') }}</a>
+                                    @if( $order->status=='completed' & $order->shipment->status =='delivering')
+
+                                        <lable>Spedito</lable>
+
+                                    @endif
+                                    @if( $order->status=='pending')
+
+                                        <lable>Modificabile</lable>
+
+                                    @endif
+                                    @if( $order->status=='completed' && $order->shipment->status =='pending')
+
+                                        <lable>In lavorazione</lable>
+
+                                    @endif
+                                    @if( $order->status=='canceled')
+
+                                        <lable>Cancellato</lable>
+
+                                    @endif
+
+
+                                </td>
+                                <td style="display: flex;flex-direction: row;justify-content: center;align-items: center;">
+
+
+
+
+                                    <div class="row list-order-action">
+                                        <div class="col-4">
+                                            <form  @if($order->canEdit()) action="{{ route('customer.orders.edit',$order->id) }}" method="post" @endif >
+                                                @csrf
+                                                <button type="submit" class="btn btn-success btn-sm" @if( $order->status !='pending') disabled @endif> <i class="fa fa-pen"></i> </button>
+                                            </form>
+                                        </div>
+                                        <div class="col-4">
+                                            <a @if($order->isInvoiceAvailable()) href="{{ route('customer.print-order', $order->id) }}" @endif
+                                            class="btn btn-primary btn-sm" @if(!$order->isInvoiceAvailable()) disabled @endif><i class="fa fa-print"></i></a>
+                                        </div>
+                                        <div class="col-4">
+                                            <a class="btn btn-info btn-sm" href="{{ route('customer.orders.view', $order->id) }}"><i class="fa fa-eye"></i></a>
+                                        </div>
+                                    </div>
+
+
                                 </td>
                             </tr>
                         @empty
