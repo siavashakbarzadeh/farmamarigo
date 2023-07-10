2adf9b41f3494f9c6583b97b7def55ea0a14a220
mralikeshtkar <alikeshtkar262@gmail.com>
2023-07-10 09:58:03 +0330

    123

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,10 @@ use Illuminate\Support\Facades\Mail;
                     ->put('u_id', $item->id)
                     ->forget(['id', 'original_price', 'front_sale_price', 'product_collections'])
                     ->mapWithKeys(function ($item, $key) {
-                        if (is_string($item) && strtotime($item) > strtotime(0) &&$date = \Illuminate\Support\Carbon::createFromTimeString($item)) {
-                            $item=$date->format('Y-m-d H:i:s');
-                        }
-                        if (is_array($item)) $item = collect($item)->toJson();
-                        if (is_object($item) && method_exists($item, 'getValue')) $item = $item->getValue();
+                        if (is_string($item) && strtotime($item) > strtotime(0) && $date = \Illuminate\Support\Carbon::createFromTimeString($item)) {
+                            $item = $date->format('Y-m-d H:i:s');
+                        } elseif (is_array($item)) $item = collect($item)->toJson();
+                        else if (is_object($item) && method_exists($item, 'getValue')) $item = $item->getValue();
                         return [$key => $item];
                     })->toArray();
                 dd($item);
