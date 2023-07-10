11c681a3f542f27f6ea0558a11078e0ac465bc29
mralikeshtkar <alikeshtkar262@gmail.com>
2023-07-10 09:56:24 +0330

    123

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,8 @@ use Illuminate\Support\Facades\Mail;
                     ->put('u_id', $item->id)
                     ->forget(['id', 'original_price', 'front_sale_price', 'product_collections'])
                     ->mapWithKeys(function ($item, $key) {
-                        if ($date = \Illuminate\Support\Carbon::createFromTimeString($item)) {
-                            dump($date);
+                        if (is_string($item) && strtotime($item) > strtotime(0) &&$date = \Illuminate\Support\Carbon::createFromTimeString($item)) {
+                            dump($item,$date);
                         }
                         if (is_array($item)) $item = collect($item)->toJson();
                         if (is_object($item) && method_exists($item, 'getValue')) $item = $item->getValue();
