21f868c098a91cf3aa1c4c77aa9992b942164f6d
mralikeshtkar <alikeshtkar262@gmail.com>
2023-08-17 11:59:54 +0330

    a123

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\CheckAuthCustomer;
 use Botble\Base\Middleware\LanguageManager;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check.auth.customer' => CheckAuthCustomer::class,
     ];
 }
