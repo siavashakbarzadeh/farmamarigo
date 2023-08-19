1ce6b79be65f25e18fe6f1f8dd05eacabd758c27
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-08-19 13:00:21 +0200

    Update overview.blade.php

--- platform/themes/wowy/views/ecommerce/customers/overview.blade.php
+++ platform/themes/wowy/views/ecommerce/customers/overview.blade.php
@@ -1,9 +1,9 @@
 @extends(Theme::getThemeNamespace() . '::views.ecommerce.customers.master')
 @section('content')
     <div class="card">
-        <div class="card-header">
-            <h5 class="mb-0">{{ __('Hello :name!', ['name' => auth('customer')->user()->name]) }} </h5>
-        </div>
+{{--        <div class="card-header">--}}
+{{--            <h5 class="mb-0">{{ __('Hello :name!', ['name' => auth('customer')->user()->name]) }} </h5>--}}
+{{--        </div>--}}
         <div class="card-body">
             <p>
                 {!! BaseHelper::clean(__('From your account dashboard. you can easily check &amp; view your <a href=":order">recent orders</a>', [
