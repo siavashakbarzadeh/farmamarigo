e0f4022708d6802a12dc828d39e9f91256975485
mralikeshtkar <alikeshtkar262@gmail.com>
2023-08-24 09:45:50 +0330

    123

--- platform/plugins/ecommerce/resources/views/orders/checkout.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/checkout.blade.php
@@ -129,7 +129,7 @@
 
                             @if (! is_plugin_active('marketplace'))
                                 @if (Arr::get($sessionCheckoutData, 'is_available_shipping', true))
-                                    <div id="shipping-method-wrapper">
+                                    <div id="shipping-method-wrapper d-none">
                                         <h5 class="checkout-payment-title">{{ __('Shipping method') }}</h5>
                                         <div class="shipping-info-loading" style="display: none;">
                                             <div class="shipping-info-loading-content">
