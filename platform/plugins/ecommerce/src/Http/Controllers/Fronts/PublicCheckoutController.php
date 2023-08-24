626b4657d37d20a2f944c7ae0389c67d9ac0e897
mralikeshtkar <alikeshtkar262@gmail.com>
2023-08-24 09:43:08 +0330

    123

--- platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCheckoutController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCheckoutController.php
@@ -111,7 +111,6 @@ class PublicCheckoutController
         HandleApplyPromotionsService $applyPromotionsService
     )
     {
-        dd("salam");
         if (!EcommerceHelper::isCartEnabled()) {
             abort(404);
         }
