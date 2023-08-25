c7b4e3b7d52fbaef52b4fbd12455bf400d20300a
mralikeshtkar <alikeshtkar262@gmail.com>
2023-08-25 12:30:05 +0330

    123

--- platform/plugins/ecommerce/src/Http/Controllers/Customers/PublicController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Customers/PublicController.php
@@ -216,7 +216,7 @@ class PublicController extends Controller
         if (!$order) {
             abort(404);
         }
-        return $response->setNextUrl(route('public.cart'));
+        return $response->setNextUrl(route('public.cart'))->setMessage(__('Update profile successfully!'));
         foreach ($order->products as $orderProduct) {
             $product = $this->productRepository->findById($orderProduct->product->id);
             if ($product->variations->count() > 0 && !$product->is_variation) {
