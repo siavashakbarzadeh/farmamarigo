3dd3250147e0afb92cefa55d5804faa54f6e6ad6
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-10-30 09:03:38 +0100

    gsggnf

--- resources/views/mails/verification-account.blade.php
+++ resources/views/mails/verification-account.blade.php
@@ -1,23 +1,22 @@
 <!doctype html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-=======
+
     <title>Document</title>
->>>>>>> parent of 500dbc7c (etwwte)
+
 </head>
 <body>
-<<<<<<< HEAD
-=======
+
 <p>Vi preghiamo di confermare la vostra richiesta di iscrizione premendo il seguente link:</p>
 
 <a href="{{ $url }}" target="_blank">{{ $url }}</a>
->>>>>>> 13c2e844e9015403136d12e41024e91768b57c97
+
 </body>
 </html>
