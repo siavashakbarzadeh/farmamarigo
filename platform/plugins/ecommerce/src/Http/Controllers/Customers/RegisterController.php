3c7582cf82dabe33ee8998f31657c04f1cd6a002
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-10-31 11:20:24 +0100

    erye

--- platform/plugins/ecommerce/src/Http/Controllers/Customers/RegisterController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Customers/RegisterController.php
@@ -110,6 +110,7 @@ class RegisterController extends Controller
 
     protected function create(array $data)
     {
+        dd($data);
         return $this->customerRepository->create([
             'name' => BaseHelper::clean($data['name']),
             'email' => BaseHelper::clean($data['email']),
