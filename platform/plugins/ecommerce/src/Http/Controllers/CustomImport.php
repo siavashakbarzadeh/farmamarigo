b38a2e13a81fb510ae4835ba86baf36115314c29
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-02 11:23:48 +0100

    sgsdg

--- platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
+++ platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
@@ -23,9 +23,95 @@ use Illuminate\Support\Facades\DB;
 
 class CustomImport extends BaseController
 {
-    public function importProducts()
-    {
+    public function users() {
+        dd('ok');
+//        $this->agents();
 
+        $users = DB::connection('mysql2')->select('select * from cli_cliente where tipologia IN ( 30,31, 32, 33,34,) and email IS NOT NULL');
+        dd($users);
+        foreach($users as $user) {
+            $exists = DB::connection('mysql')->table('ec_customers')->where('codice', $user->codice)->exists();
+
+            if (!$exists && $user->email) {
+                $password = $this->generateRandomString();  // Generate the password only for new users
+
+                $row=DB::connection('mysql')->table('ec_customers')->insert([
+                    'id'=>$user->pk_cliente_id,
+                    'codice' => $user->codice,
+                    'name' => $user->nome,
+                    'codice_fiscale' => $user->codice_fiscale,
+                    'piva' => $user->piva,
+                    'password' => bcrypt($password),
+                    'email' => $user->email,
+                    'agent_id' => $user->fk_agente_id,
+                    'region_id' => $user->fk_regione_id,
+                    'pec' => $user->pec,
+                    'flag_isola'=>$user->flag_isola
+                ]);
+
+
+
+                $provincia=DB::connection('mysql2')->table('arc_provincia')->where('pk_provincia_id', $user->fk_provincia_id)->first();
+                $regione=DB::connection('mysql2')->table('arc_regione')->where('pk_regione_id', $user->fk_regione_id)->first();
+
+                DB::connection('mysql')->table('ec_customer_addresses')->insert([
+                    'customer_id' => $user->pk_cliente_id,
+                    'phone' => '0000000000',
+                    'email' => $user->email?$user->email:'null@null.com',
+                    'country' => "IT",
+                    'zip_code' => $user->cap ? $user->cap:'00000',
+                    'name' => $user->nome,
+                    'is_default' => 1,
+                    'city' => ($user->fk_regione_id)?$regione->nome:'NULL',
+                    'address' => "default",
+                    'state' => ($user->fk_provincia_id)?$provincia->nome:'NULL'
+                ]);
+
+                // Dispatch the Welcome job
+                if($user->email){
+                    Mail::to($user->email)->send(new Welcome($user->nome,$user->email,$user->codice,$password));
+                    Mail::to("a.allahverdi@icoa.it")->send(new Welcome($user->nome,$user->email,$user->codice,$password));
+                }
+            }elseif($exists){
+
+                $row=DB::connection('mysql')->table('ec_customers')->where('codice', $user->codice)->update([
+                    'codice' => $user->codice,
+                    'name' => $user->nome,
+                    'codice_fiscale' => $user->codice_fiscale,
+                    'piva' => $user->piva,
+                    'email' => $user->email,
+                    'agent_id' => $user->fk_agente_id,
+                    'region_id' => $user->fk_regione_id,
+                    'pec' => $user->pec,
+                    'flag_isola'=>$user->flag_isola
+                ]);
+
+                $provincia=DB::connection('mysql2')->table('arc_provincia')->where('pk_provincia_id', $user->fk_provincia_id)->first();
+                $regione=DB::connection('mysql2')->table('arc_regione')->where('pk_regione_id', $user->fk_regione_id)->first();
+
+                DB::connection('mysql')->table('ec_customer_addresses')->where('customer_id', $user->pk_cliente_id)->update([
+                    'phone' => '0000000000',
+                    'email' => $user->email?$user->email:'null@null.com',
+                    'country' => "IT",
+                    'zip_code' => $user->cap ? $user->cap:'00000',
+                    'name' => $user->nome,
+                    'is_default' => 1,
+                    'city' => ($user->fk_regione_id)?$regione->nome:'NULL',
+                    'address' => "default",
+                    'state' => ($user->fk_provincia_id)?$provincia->nome:'NULL'
+                ]);
+
+            }
+        }
+
+        // Remove the users that are no longer in the source
+        $sourceIds = array_column($users, 'codice');
+        $allUserIds = DB::connection('mysql')->table('ec_customers')->pluck('codice')->toArray();
+        $usersToRemove = array_diff($allUserIds, $sourceIds);
+        DB::connection('mysql')->table('ec_customers')->whereIn('codice', $usersToRemove)->update(['status' => 'locked']);
+
+        $usersUpdatedCount = count($users);
+        return view('plugins/ecommerce::customImport.clienti-foreign-keys', compact('usersUpdatedCount'));
     }
 
     public function sconto()
