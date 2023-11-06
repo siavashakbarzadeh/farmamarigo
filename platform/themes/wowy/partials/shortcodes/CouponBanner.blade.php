974be9b3d6f99bc21a428967a9ee4c64648b94ff
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-06 10:38:38 +0100

    sdgsg

--- platform/themes/wowy/partials/shortcodes/CouponBanner.blade.php
+++ platform/themes/wowy/partials/shortcodes/CouponBanner.blade.php
@@ -1,9 +1,9 @@
 
 
-<div class="container-fluid percent-texture mt-5" style="position: relative;overflow:hidden" id="linee">
+<div class="container-fluid percent-texture " style="position: relative;overflow:hidden" id="linee">
     <div class="row row-10">
 
-            <div class="col" style="margin:0 !important;height:fit-content;background-color:rgb(81, 180, 72);display:flex;align-items:center;justify-content:center;color:white;min-height:120px;padding:20px">
+            <div class="col" style="margin-top:15px !important;height:fit-content;background-color:rgb(81, 180, 72);display:flex;align-items:center;justify-content:center;color:white;min-height:120px;padding:20px">
                 <h4 style="color:white;line-height:normal;text-transform:uppercase">{!! BaseHelper::clean($shortcode->text) !!} <span style="    padding: 5px;
                     border-radius: 4px;line-height:normal;
                     background: #E52728;"> Codice Coupon: {!! BaseHelper::clean($shortcode->coupon) !!}</span></a>
