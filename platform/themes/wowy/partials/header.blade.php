2c83d863b5ac89a728389b372dcf0059c110d4d8
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-10 11:40:23 +0100

    Update header.blade.php

--- platform/themes/wowy/partials/header.blade.php
+++ platform/themes/wowy/partials/header.blade.php
@@ -101,7 +101,7 @@
                         @php $currencies = is_plugin_active('ecommerce') ? get_all_currencies() : []; @endphp
 
                         @if (is_plugin_active('ecommerce') || is_plugin_active('language'))
-                            <div class="d-flex justify-content-end align-items-center w-25 bg-danger">
+                            <div class="d-flex justify-content-end align-items-center col-4 float-end bg-danger">
                                 <div class="header-info header-info-right">
                                         <ul>
                                             @if (is_plugin_active('language'))
