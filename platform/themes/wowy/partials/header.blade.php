48046ca60d27b4204a468c0c1cd0884b59c602a4
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-10 11:26:34 +0100

    rw523

--- platform/themes/wowy/partials/header.blade.php
+++ platform/themes/wowy/partials/header.blade.php
@@ -69,7 +69,7 @@
                             </div>
                         </div>
 
-                        <div class="col-xl-5 col-lg-4">
+                        <div class="col-xl-4 col-lg-4">
                             <div class="text-center">
                                 @if (theme_option('header_messages'))
                                     <div id="news-flash" class="d-inline-block">
