60b9eb5a4e7f63011d32a98386a37cfbfb6f4af8
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-13 09:37:45 +0100

    fjfgj

--- platform/plugins/ecommerce/src/Http/Controllers/Customers/RegisterController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Customers/RegisterController.php
@@ -78,7 +78,7 @@ class RegisterController extends Controller
     {
         $rules = [
             'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:ec_customers,email',
+            'email' => 'required|email|max:255|unique:ec_customers',
             'password' => 'required|min:6|confirmed',
         ];
 
