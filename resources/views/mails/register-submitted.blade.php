3e65dd57bbe7d5fe4db06b1b1f2bb3207f217255
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-20 10:27:35 +0100

    Update register-submitted.blade.php

--- resources/views/mails/register-submitted.blade.php
+++ resources/views/mails/register-submitted.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Registration of #10000{{$customer->id}}</title>
+{{--    <title>Registration of #10000{{$customer->id}}</title>--}}
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
