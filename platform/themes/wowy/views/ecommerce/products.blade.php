ff26a6bc810652b032038ae98477307d89e9fe36
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-21 14:29:53 +0100

    Update products.blade.php

--- platform/themes/wowy/views/ecommerce/products.blade.php
+++ platform/themes/wowy/views/ecommerce/products.blade.php
@@ -19,9 +19,9 @@
 <div class="col-lg-12">
     <a class="shop-filter-toogle" href="#">
         <span class="fal fa-2x bg-danger fa-filter mr-5  ml-0"></span>
-        <span class="title">{{ __('Filters') }}</span>
-        <i class="far fa-angle-up fa-xl angle-down"></i>
-        <i class="far fa-angle-down  fa-xl angle-up"></i>
+        <span class="title fa-2x">{{ __('Filters') }}</span>
+        <i class="far fa-2x fa-angle-up  angle-down"></i>
+        <i class="far fa-2x fa-angle-down  fa-2x angle-up"></i>
     </a>
     <form action="{{ URL::current() }}" method="GET" id="products-filter-ajax">
         @if ($layout != 'product-full-width')
