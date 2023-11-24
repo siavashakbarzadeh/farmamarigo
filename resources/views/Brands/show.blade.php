ae46fb932ecf2376fd771d08239d8a4264a0546c
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-24 12:15:36 +0100

    Update show.blade.php

--- resources/views/Brands/show.blade.php
+++ resources/views/Brands/show.blade.php
@@ -8,17 +8,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
+<div class="container mt-5">
     <div class="row">
-        @foreach($cards as $card)
-            <div class="col-md-3">
+        @foreach ($cards as $card)
+            <div class="col-md-3 mb-4">
                 <div class="card">
                     <img src="{{ asset('images/' . $card['image']) }}" class="card-img-top" alt="{{ $card['title'] }}">
                     <div class="card-body">
                         <h5 class="card-title">{{ $card['title'] }}</h5>
                         <p class="card-text">{{ $card['description'] }}</p>
-                        <a href="#" class="btn btn-primary">Button 1</a>
-                        <a href="#" class="btn btn-secondary">Button 2</a>
+                        <a href="#" class="btn btn-primary">SCARICA IL CATALOGO</a>
+                        <a href="#" class="btn btn-secondary">RICHIEDI INFORMAZIONI</a>
                     </div>
                 </div>
             </div>
