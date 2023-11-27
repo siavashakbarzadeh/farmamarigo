24c9f7f269ac77687ed56df95f49193f53baed64
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-27 10:00:08 +0100

    tutu

--- platform/themes/wowy/partials/shortcodes/marchi.blade.php
+++ platform/themes/wowy/partials/shortcodes/marchi.blade.php
@@ -1,44 +1,44 @@
 @php
-    $brands = Theme\Wowy\Http\Controllers\WowyController::ajaxGetFeaturedBrands();
+    $brands = Theme\Wowy\Http\Controllers\WowyController::ajaxGetMarchi();
 @endphp
-<section class="section-padding-60">
-    <div class="container">
-        <h3 class="section-title style-1 mb-30 wow fadeIn animated">{!! BaseHelper::clean($title) !!}</h3>
-        {{--        <div id="owl-demo" class="owl-carousel owl-theme">--}}
-        <div class=" owl-carousel owl-theme featured-brands-carousel ">
-            {{--        <div class=" owl-carousel owl-theme brands-carousel ">--}}
+{{--<section class="section-padding-60">--}}
+{{--    <div class="container">--}}
+{{--        <h3 class="section-title style-1 mb-30 wow fadeIn animated">{!! BaseHelper::clean($title) !!}</h3>--}}
+{{--        --}}{{--        <div id="owl-demo" class="owl-carousel owl-theme">--}}
+{{--        <div class=" owl-carousel owl-theme featured-brands-carousel ">--}}
+{{--            --}}{{--        <div class=" owl-carousel owl-theme brands-carousel ">--}}
 
-            @foreach ($brands as $brand)
-                <div class="col-6">
-                    <a class="displayManufacturer" href="{{ $brand->website }}">
-                        <img class="displayManufacturerImg" src="{{ RvMedia::getImageUrl($brand->logo ) }}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
-        {{-- <featured-brands-component url="{{ route('public.ajax.featured-brands') }}"></featured-brands-component> --}}
-    </div>
-</section>
-
-{{--<div class="container-fluid mt-5">--}}
-{{--    <div class="row">--}}
-{{--        @foreach ($cards as $card)--}}
-{{--            <div class="col-md-3 mb-4">--}}
-{{--                <div class="card">--}}
-{{--                    <img src="{{ $card['image'] }}" class="card-img-top" alt="{{ $card['title'] }}">--}}
-{{--                    <div class="card-body">--}}
-{{--                        <h5 class="card-title">{{ $card['title'] }}</h5>--}}
-{{--                        <hr>--}}
-{{--                        --}}{{--                        <p class="card-text">{{ $card['description'] }}</p>--}}
-{{--                        <a href="https://www.marigopharma.it/wp-content/uploads/2018/02/Cat_ProntoLeggo_2023_WEB.pdf" target="_blank" class="custom-link btn btn-md btn-accent btn-outline btn-icon-left btn-block">SCARICA IL CATALOGO <i class="fa fa-download"></i></a>--}}
-{{--                        <a href="https://www.marigopharma.it/richiesta-informazioni-prodotti-per-farmacie-e-parafarmacie/" tabindex="-1" class="pushed custom-link btn btn-md btn-accent btn-outline btn-icon-left btn-block">RICHIEDI INFORMAZIONI <i class="fa fa-envelope"></i></a>--}}
-
-{{--                        --}}{{--                        <a href="#" class="btn btn-primary">SCARICA IL CATALOGO</a>--}}
-{{--                        --}}{{--                        <a href="#" class="btn btn-secondary">RICHIEDI INFORMAZIONI</a>--}}
-{{--                    </div>--}}
+{{--            @foreach ($brands as $brand)--}}
+{{--                <div class="col-6">--}}
+{{--                    <a class="displayManufacturer" href="{{ $brand->website }}">--}}
+{{--                        <img class="displayManufacturerImg" src="{{ RvMedia::getImageUrl($brand->logo ) }}">--}}
+{{--                    </a>--}}
 {{--                </div>--}}
-{{--            </div>--}}
-{{--        @endforeach--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--        --}}{{-- <featured-brands-component url="{{ route('public.ajax.featured-brands') }}"></featured-brands-component> --}}
 {{--    </div>--}}
-{{--</div>--}}
+{{--</section>--}}
+
+<div class="container-fluid mt-5">
+    <div class="row">
+        @foreach ($cards as $card)
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img src="{{ $card['image'] }}" class="card-img-top" alt="{{ $card['title'] }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $card['title'] }}</h5>
+                        <hr>
+                                                <p class="card-text">{{ $card['description'] }}</p>
+                        <a href="https://www.marigopharma.it/wp-content/uploads/2018/02/Cat_ProntoLeggo_2023_WEB.pdf" target="_blank" class="custom-link btn btn-md btn-accent btn-outline btn-icon-left btn-block">SCARICA IL CATALOGO <i class="fa fa-download"></i></a>
+                        <a href="https://www.marigopharma.it/richiesta-informazioni-prodotti-per-farmacie-e-parafarmacie/" tabindex="-1" class="pushed custom-link btn btn-md btn-accent btn-outline btn-icon-left btn-block">RICHIEDI INFORMAZIONI <i class="fa fa-envelope"></i></a>
+
+                                                <a href="#" class="btn btn-primary">SCARICA IL CATALOGO</a>
+                                                <a href="#" class="btn btn-secondary">RICHIEDI INFORMAZIONI</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
--- platform/themes/wowy/src/Http/Controllers/WowyController.php
+++ platform/themes/wowy/src/Http/Controllers/WowyController.php
@@ -128,6 +128,35 @@ class WowyController extends PublicController
 
         return $brands;
     }
+    public static function ajaxGetMarchi()
+    {
+        $cards = [
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/copertina-prontoleggo.jpg', 'title' => 'Prontoleggo – Occhiali da lettura', ],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/evi-brand-italia.jpg', 'title' => 'Brand Italia – Linea antizanzare, maschere viso e linea arnica', 'description' => 'Description for card 2'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/nuvita.jpg', 'title' => 'Nuvita – Puericultura Leggera', 'description' => 'Description for card 3'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/evi-petformance.jpg', 'title' => 'Petformance – Articoli per la salute, il benessere e l’igiene di cani e gatti', 'description' => 'Description for card 4'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/test-rapidi.jpg', 'title' => 'Test Rapidi Professionali e Self Test', 'description' => 'Description for card 4'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/mix-mascherine.jpg', 'title' => 'Mascherine Protettive – FFP2 e Chirurgiche', 'description' => 'Description for card 4'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/img-termoscanner-pusiossimetri-catalogo-marigo.jpg', 'title' => 'Termoscanner e Pulsossimetri', 'description' => 'Description for card 4'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/cat-accessories.jpg', 'title' => 'Beautytime – Make up', 'description' => 'Description for card 4'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/cat-cosmetics.jpg', 'title' => 'Beautytime – Linea viso e Detersione', 'description' => 'Description for card 4'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/cat-make-up.jpg', 'title' => 'Beautytime – Accessori', 'description' => 'Description for card 4'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/cat-rose-gold.jpg', 'title' => 'Beautytime – Gold rose', 'description' => 'Description for card 11'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/cat-smokey-eye.jpg', 'title' => 'Beautytime – Smokey eye', 'description' => 'Description for card 11'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/cat-travel-set.jpg', 'title' => 'Beautytime – Travel set', 'description' => 'Description for card 11'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/cat-hair-accesories.jpg', 'title' => 'Beautytime – Lookrezia', 'description' => 'Description for card 11'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/cat-lime.jpg', 'title' => 'Beautytime – Lime personalizzate', 'description' => 'Description for card 11'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/copertina-pasante-1.jpg', 'title' => 'Pasante – Profilattici', 'description' => 'Description for card 11'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/rowo-compresse-caldo-freddo.jpg', 'title' => 'Röwo – Compresse caldo freddo', 'description' => 'Description for card 11'],
+            ['image' => 'https://marigopharma.marigo.collaudo.biz/storage/copertina-prontoleggo-sunglasses.jpg', 'title' => 'Prontoleggo – Sunglasses', 'description' => 'Description for card 11'],
+
+            // Add more cards as needed
+        ];
+
+//        $categories = \Botble\Ecommerce\Models\ProductCategory::all();
+//        return view('Brands.show', compact('cards',));
+        return $cards;
+    }
     /**
      * @param int $id
      * @param Request $request
