8b3b49e201a2725b1c504c11d6fc7739b91a0192
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-27 09:44:37 +0100

    Update show.blade.php

--- resources/views/Brands/show.blade.php
+++ resources/views/Brands/show.blade.php
@@ -43,10 +43,6 @@
 {{--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>--}}
 {{--</body>--}}
 {{--</html>--}}
-@php
-use Theme;
-Theme::layout('default');
-@endphp
 
 
         <div class="container-fluid mt-5">
