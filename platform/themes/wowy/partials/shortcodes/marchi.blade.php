158e94c8847e56b3e3bb3f66ffd87c99125f01ec
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-11-27 10:10:16 +0100

    Update marchi.blade.php

--- platform/themes/wowy/partials/shortcodes/marchi.blade.php
+++ platform/themes/wowy/partials/shortcodes/marchi.blade.php
@@ -29,7 +29,7 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ $card['title'] }}</h5>
                         <hr>
-                                                <p class="card-text">{{ $card['description'] }}</p>
+{{--                                                <p class="card-text">{{ $card['description'] }}</p>--}}
                         <a href="https://www.marigopharma.it/wp-content/uploads/2018/02/Cat_ProntoLeggo_2023_WEB.pdf" target="_blank" class="custom-link btn btn-md btn-accent btn-outline btn-icon-left btn-block">SCARICA IL CATALOGO <i class="fa fa-download"></i></a>
                         <a href="https://www.marigopharma.it/richiesta-informazioni-prodotti-per-farmacie-e-parafarmacie/" tabindex="-1" class="pushed custom-link btn btn-md btn-accent btn-outline btn-icon-left btn-block">RICHIEDI INFORMAZIONI <i class="fa fa-envelope"></i></a>
 
