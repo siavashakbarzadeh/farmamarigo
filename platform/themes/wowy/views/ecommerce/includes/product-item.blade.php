ce6db0417047e7a96a96bb9af425779f0a4dd14b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-12-03 19:53:44 +0200

    Update product-item.blade.php

--- platform/themes/wowy/views/ecommerce/includes/product-item.blade.php
+++ platform/themes/wowy/views/ecommerce/includes/product-item.blade.php
@@ -56,10 +56,8 @@
             <div class="product-price">
                 <span>{{ format_price($product->front_sale_price_with_taxes) }}</span>
                 @php
-                dd(session()->all());
-                if(auth()->user('customer')!==NULL){
-                    dd(auth()->user('customer')->id);
-                    $userid=request()->user('customer')->id;
+                if(auth('customer')->user()!==NULL){
+                    $userid=auth('customer')->user()->id;
                     dd($userid);
                     $pricelist=DB::connection('mysql')->select("select * from ec_pricelist where product_id=$product->id and customer_id=$userid");
                     if(isset($pricelist[0])){
