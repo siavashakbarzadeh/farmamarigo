3fe72ba7f097c293c811f8f4c72d4aed01e37ff5
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-12-03 19:45:20 +0200

    Update product-item.blade.php

--- platform/themes/wowy/views/ecommerce/includes/product-item.blade.php
+++ platform/themes/wowy/views/ecommerce/includes/product-item.blade.php
@@ -56,7 +56,7 @@
             <div class="product-price">
                 <span>{{ format_price($product->front_sale_price_with_taxes) }}</span>
                 @php
-                dd(auth()->user());
+                dd(request()->user());
                 if(auth()->user('customer')!==NULL){
                     dd(auth()->user('customer')->id);
                     $userid=request()->user('customer')->id;
