0ef39be24338adbc5066f4004c7ed93aa5c8c858
Amirezalla <allahverdiamirreza@gmail.com>
2023-12-04 14:01:36 +0100

    Revert "Revert "generate product sku fixed""
    This reverts commit bc617de61392487a8d98969e491e19a8a79ab11e.

--- platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
+++ platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
@@ -299,6 +299,7 @@ class CustomImport extends BaseController
                     $product_name = str_replace('&', 'and', trim($productsWithoutVariant['nome']));
                     $price = $productsWithoutVariant['prezzo'];
                     $taxId = $productsWithoutVariant['fk_codice_iva_id'];
+                    $sku=$productsWithoutVariant['codice'];
                     if (in_array($product_name, $items)) {
                         $productItem = \Botble\Ecommerce\Models\Product::query()->where('name', $product_name)->first();
                         $productItem->update([
@@ -306,9 +307,10 @@ class CustomImport extends BaseController
                             'price' => $price,
                             'tax_id' => $taxId,
                             'images' => collect([strtolower($productsWithoutVariant['codice']) . '.jpg'])->toJson(),
+                            'sku'=>$sku,
                         ]);
                     }else{
-                        $productItem = $this->_generateProduct($product_name,$productsWithoutVariant,$price,$brands,$taxId);
+                        $productItem = $this->_generateProduct($product_name,$productsWithoutVariant,$price,$brands,$taxId,$sku);
                         // product_id va tax_id be table ec_tax_products ezafe shavad
                         $this->_generateTranslationProduct($product_name,$productItem);
                         $this->_generateSlugProduct($product_name,$productItem);
@@ -362,6 +364,7 @@ class CustomImport extends BaseController
                     $product = collect($products)->first();
                     $price = $product ? $product['prezzo'] : 0;
                     $taxId= $product['fk_codice_iva_id'];
+                    $sku=$product['codice'];
                     if (in_array(str_replace('&', 'and', trim($product_name)), $items)) {
                         $productItem = \Botble\Ecommerce\Models\Product::query()->where('name', str_replace('&', 'and', trim($product_name)))->first();
                         $productItem->update([
@@ -371,7 +374,7 @@ class CustomImport extends BaseController
                             'images' => collect([strtolower($product['codice']) . '.jpg'])->toJson(),
                         ]);
                     } else {
-                        $productItem = $this->_generateProduct($product_name,$product,$price,$brands,$taxId);
+                        $productItem = $this->_generateProduct($product_name,$product,$price,$brands,$taxId,$sku);
                         $this->_generateTranslationProduct($product_name,$productItem);
                         $this->_generateSlugProduct($product_name,$productItem);
                         if ($variationItems->count()) {
@@ -459,7 +462,7 @@ class CustomImport extends BaseController
 
     }
 
-    private function _generateProduct($product_name,$product,$price,$brands,$taxId)
+    private function _generateProduct($product_name,$product,$price,$brands,$taxId,$sku)
     {
         $productItem= \Botble\Ecommerce\Models\Product::query()->create([
             'name' => str_replace('&', 'and', trim($product_name)),
@@ -468,6 +471,7 @@ class CustomImport extends BaseController
             'tax_id' => $taxId,
             'brand_id' => \Botble\Ecommerce\Models\Brand::where('name', $brands->toArray()[$product['fk_fornitore_id']])->first()->id,
             'images' => collect([strtolower($product['codice']) . '.jpg'])->toJson(),
+            'sku'=>$sku,
         ]);
         $tax = Tax::find($taxId);
         if ($tax){
