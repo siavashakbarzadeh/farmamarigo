88b2f2e731009ea6ce24da9208334fec676c455d
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-12-05 12:06:29 +0100

    4646

--- platform/themes/wowy/partials/shortcodes/pricelist-product-collections.blade.php
+++ platform/themes/wowy/partials/shortcodes/pricelist-product-collections.blade.php
@@ -1,6 +1,6 @@
 @if (auth('customer'))
-<section class="product-tabs pt-40 pb-30 wow fadeIn animated">
-    <product-collections-component title="{!! BaseHelper::clean($title) !!}"
- :product_collections="{{ json_encode($productCollections) }}" url="{{ route('public.ajax.products') }}"></product-collections-component>
-</section>
+{{--<section class="product-tabs pt-40 pb-30 wow fadeIn animated">--}}
+{{--    <product-collections-component title="{!! BaseHelper::clean($title) !!}"--}}
+{{-- :product_collections="{{ json_encode($productCollections) }}" url="{{ route('public.ajax.products') }}"></product-collections-component>--}}
+{{--</section>--}}
 @endif
