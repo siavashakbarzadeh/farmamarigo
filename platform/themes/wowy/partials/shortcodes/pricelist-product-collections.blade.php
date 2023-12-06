c02e6bfea48bb5c7c7d7b3f5fa0efc7ee6b3b4cf
siavashakbarzadeh1 <91117383+siavashakbarzadeh1@users.noreply.github.com>
2023-12-06 16:45:11 +0100

    Update pricelist-product-collections.blade.php

--- platform/themes/wowy/partials/shortcodes/pricelist-product-collections.blade.php
+++ platform/themes/wowy/partials/shortcodes/pricelist-product-collections.blade.php
@@ -25,7 +25,37 @@ use App\Http\Controllers\PricelistController;
             <div class="col-lg-3 col-md-4">
                 <div class="product-cart-wrap mb-30">
                     <div class="product-img-action-wrap">
-                        <div class="product-img product-img-zoom"><a href="https://marigopharma.marigo.collaudo.biz/products/biberon-250-ml-cool-azzurro"><img src="https://marigopharma.marigo.collaudo.biz/storage/5350555012641-400x400.jpg" alt="Biberon 250 ml COOL AZZURRO" class="default-img"> <img src="https://marigopharma.marigo.collaudo.biz/storage/5350555012641-400x400.jpg" alt="Biberon 250 ml COOL AZZURRO" class="hover-img"></a></div> <div class="product-badges product-badges-position product-badges-mrg"></div></div> <div class="product-content-wrap"><div class="product-category"><a href="https://marigopharma.marigo.collaudo.biz">infanzia</a></div> <h2><a href="https://marigopharma.marigo.collaudo.biz/products/biberon-250-ml-cool-azzurro">Biberon 250 ml COOL AZZURRO</a></h2> <div class="rating_wrap"><div class="rating"><div class="product_rate" style="width: 0%;"></div></div> <span class="rating_num">(0)</span></div> <div class="product-price"><span>9,49€</span></div> <div class="product-action-1 show "><a aria-label="Aggiungi" data-id="18703" data-url="https://marigopharma.marigo.collaudo.biz/cart/add-to-cart" href="#" class="action-btn hover-up add-to-cart-button"><i class="far fa-shopping-bag"></i></a></div></div></div></div>
+                        <div class="product-img product-img-zoom">
+                            <a href="">
+                                <img src="{{ RvMedia::getImageUrl($product->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $product->name }}" class="default-img">
+                                <img src="{{ RvMedia::getImageUrl($product->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $product->name }}" class="hover-img">
+                            </a>
+                        </div>
+                        <div class="product-badges product-badges-position product-badges-mrg">
+
+                        </div>
+                    </div>
+                    <div class="product-content-wrap">
+                        <div class="product-category">
+{{--                            <a href="https://marigopharma.marigo.collaudo.biz">infanzia</a>--}}
+                        </div>
+                        <h2><a href="">{{ $product->name }}</a></h2>
+                        <div class="rating_wrap">
+                            <div class="rating">
+                                <div class="product_rate" style="width: 0%;">
+
+                                </div>
+                            </div>
+                            <span class="rating_num">(0)</span>
+                        </div> <div class="product-price">
+                            <span>{{ format_price($product->price) }}</span>
+                        </div>
+                        <div class="product-action-1 show ">
+                            <a aria-label="Aggiungi" data-id="18703" data-url="" href="#" class="action-btn hover-up add-to-cart-button"><i class="far fa-shopping-bag"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 {{--@else--}}
