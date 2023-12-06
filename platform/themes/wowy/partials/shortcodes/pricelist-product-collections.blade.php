7dbd93e8baf9d9a976d3126dae4ec56f6da5f64a
siavashakbarzadeh1 <91117383+siavashakbarzadeh1@users.noreply.github.com>
2023-12-06 16:47:06 +0100

    Update pricelist-product-collections.blade.php

--- platform/themes/wowy/partials/shortcodes/pricelist-product-collections.blade.php
+++ platform/themes/wowy/partials/shortcodes/pricelist-product-collections.blade.php
@@ -6,22 +6,7 @@ use App\Http\Controllers\PricelistController;
     <div class="container mb-4">
 
         <div class="card-deck">
-            <div class="col-lg-3">
-                @foreach($products as $product)
-                    <div class="product-item product-loop">
-                        <img src="{{ RvMedia::getImageUrl($product->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $product->name }}" class="product-item-thumb">
-                        <h3>{{ $product->name }}</h3>
-                        <span class="price">
-{{ format_price($product->price) }}
-                                </span>
-                        <div class="product-action">
-                            <a data-quantity='1' data-product='{{ $product->id }}' href="javascript: void(0);"
-                               class="btn btn-info">{{ __('Add to cart') }}</a>
-                        </div>
-                    </div>
-
-                @endforeach
-            </div>
+           
             <div class="col-lg-3 col-md-4">
                 <div class="product-cart-wrap mb-30">
                     <div class="product-img-action-wrap">
