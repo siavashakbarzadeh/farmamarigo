868c54e577540c2218aac8002e2568c5a35c626c
siavashakbarzadeh1 <91117383+siavashakbarzadeh1@users.noreply.github.com>
2023-12-06 16:48:30 +0100

    Update pricelist-product-collections.blade.php

--- platform/themes/wowy/partials/shortcodes/pricelist-product-collections.blade.php
+++ platform/themes/wowy/partials/shortcodes/pricelist-product-collections.blade.php
@@ -8,38 +8,42 @@ use App\Http\Controllers\PricelistController;
         <div class="card-deck">
            
             <div class="col-lg-3 col-md-4">
-                <div class="product-cart-wrap mb-30">
-                    <div class="product-img-action-wrap">
-                        <div class="product-img product-img-zoom">
-                            <a href="">
-                                <img src="{{ RvMedia::getImageUrl($product->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $product->name }}" class="default-img">
-                                <img src="{{ RvMedia::getImageUrl($product->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $product->name }}" class="hover-img">
-                            </a>
-                        </div>
-                        <div class="product-badges product-badges-position product-badges-mrg">
-
-                        </div>
-                    </div>
-                    <div class="product-content-wrap">
-                        <div class="product-category">
-{{--                            <a href="https://marigopharma.marigo.collaudo.biz">infanzia</a>--}}
-                        </div>
-                        <h2><a href="">{{ $product->name }}</a></h2>
-                        <div class="rating_wrap">
-                            <div class="rating">
-                                <div class="product_rate" style="width: 0%;">
-
-                                </div>
+                @foreach($products as $product)
+                    <div class="product-cart-wrap mb-30">
+                        <div class="product-img-action-wrap">
+                            <div class="product-img product-img-zoom">
+                                <a href="">
+                                    <img src="{{ RvMedia::getImageUrl($product->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $product->name }}" class="default-img">
+                                    <img src="{{ RvMedia::getImageUrl($product->image, 'thumb', false, RvMedia::getDefaultImage()) }}" alt="{{ $product->name }}" class="hover-img">
+                                </a>
                             </div>
-                            <span class="rating_num">(0)</span>
-                        </div> <div class="product-price">
-                            <span>{{ format_price($product->price) }}</span>
+                            <div class="product-badges product-badges-position product-badges-mrg">
+
+                            </div>
                         </div>
-                        <div class="product-action-1 show ">
-                            <a aria-label="Aggiungi" data-id="18703" data-url="" href="#" class="action-btn hover-up add-to-cart-button"><i class="far fa-shopping-bag"></i></a>
+                        <div class="product-content-wrap">
+                            <div class="product-category">
+                                {{--                            <a href="https://marigopharma.marigo.collaudo.biz">infanzia</a>--}}
+                            </div>
+                            <h2><a href="">{{ $product->name }}</a></h2>
+                            <div class="rating_wrap">
+                                <div class="rating">
+                                    <div class="product_rate" style="width: 0%;">
+
+                                    </div>
+                                </div>
+                                <span class="rating_num">(0)</span>
+                            </div> <div class="product-price">
+                                <span>{{ format_price($product->price) }}</span>
+                            </div>
+                            <div class="product-action-1 show ">
+                                <a aria-label="Aggiungi" data-id="18703" data-url="" href="#" class="action-btn hover-up add-to-cart-button"><i class="far fa-shopping-bag"></i></a>
+                            </div>
                         </div>
                     </div>
-                </div>
+
+                @endforeach
+
             </div>
         </div>
     </div>
