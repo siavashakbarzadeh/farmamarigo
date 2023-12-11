945cd71ff38101d0952201a2b8ef826051ffcc4f
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-12-11 13:17:17 +0200

    Update PublicCheckoutController.php

--- platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCheckoutController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCheckoutController.php
@@ -1007,7 +1007,7 @@ class PublicCheckoutController
             'status' => "COMPLETED",
             'paid_at' => $order->created_at,
             'tax_amount' => $order->tax_amount,
-            'shipping_amount' => $order->shippingAmount->shippingAmount,
+            'shipping_amount' => round_to_2dp($order->shippingAmount->shippingAmount),
             'discount_amount' => $order->discount_amount,
             'sub_total' => $order->sub_total + ($order->tax_amount),
             'amount' => $order->amount,
