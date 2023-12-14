e669135361524753a6d92d8a01b3c03ffbaaafb7
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-12-15 01:38:00 +0200

    Update filters.blade.php

--- platform/themes/wowy/views/ecommerce/includes/filters.blade.php
+++ platform/themes/wowy/views/ecommerce/includes/filters.blade.php
@@ -127,7 +127,7 @@
             @php
             $category = $_GET['categories'][0] ?? null;
             @endphp
-            <a class="btn btn-danger col-12" style="font-size:small;border-radius:25px; text-transform:none;" href="{{ '/products?categories='.$category }}">{{ __('Cancella filtri') }}</a>
+            <a class="btn btn-danger col-12" style="font-size:small;border-radius:25px; text-transform:none;" href="{{ '/products?categories='.(string)$category }}">{{ __('Cancella filtri') }}</a>
         </div>
     </div>
 </div>
