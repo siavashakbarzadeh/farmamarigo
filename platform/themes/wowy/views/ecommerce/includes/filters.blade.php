16f645eb1ddff8f135f420b4d29996b2d307b553
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-12-15 01:40:25 +0200

    Update filters.blade.php

--- platform/themes/wowy/views/ecommerce/includes/filters.blade.php
+++ platform/themes/wowy/views/ecommerce/includes/filters.blade.php
@@ -127,7 +127,7 @@
             @php
             $category = $_GET['categories'][0] ?? null;
             @endphp
-            <a class="btn btn-danger col-12" style="font-size:small;border-radius:25px; text-transform:none;" href="https://marigopharma.marigo.collaudo.biz/products?categories=439">{{ __('Cancella filtri') }}</a>
+            <a class="clear_filter dib clear_all_filter" href="{{ URL::current() }}">{{ __('Cancella filtri') }}</a>
         </div>
     </div>
 </div>
