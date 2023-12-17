fdfe69f0acae76e87d789faf857355a620aecfc7
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-12-17 13:17:42 +0100

    rty

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+//        $schedule->command('queue:work --timeout=60 --tries=2 --once')
+//            ->everyMinute()
+//            ->withoutOverlapping();
         // $schedule->command('inspire')->hourly();
     }
 
