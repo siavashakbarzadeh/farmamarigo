9829f48da9bfed7f00ecdcf69b4182e1945d1aa9
Amirezalla <allahverdiamirreza@gmail.com>
2023-12-18 11:54:19 +0100

    Update header.blade.php

--- platform/themes/wowy/partials/header.blade.php
+++ platform/themes/wowy/partials/header.blade.php
@@ -123,7 +123,7 @@
                                                 @endif
                                                 @if (auth('customer')->check())
                                                         <li>
-                                                            <a href="{{ route('customer.overview') }}">Bentornato! {{ auth('customer')->user()->name }}</a>
+                                                            <a class="profile-action-2" href="{{ route('customer.overview') }}">Bentornato! {{ auth('customer')->user()->name }}</a>
                                                             <ul class="profile-dropdown-wrap profile-dropdown-hm2">
                                                             <div class="row" style="width:100%"> <a class="bg-light item-profile-wrap" href="https://marigopharma.marigo.collaudo.biz/customer/edit-account">Profilo</a></div>
                                                                 <div class="row" style="width:100%"><a class="bg-light item-profile-wrap" href="https://marigopharma.marigo.collaudo.biz/customer/orders">Ordini</a></div>
