e3fc3070f9c5b89190530cfa83ec7d582b1f9bd7
Amirezalla <allahverdiamirreza@gmail.com>
2023-12-18 08:52:06 +0100

    Update filters.blade.php

--- platform/themes/wowy/views/ecommerce/includes/filters.blade.php
+++ platform/themes/wowy/views/ecommerce/includes/filters.blade.php
@@ -88,8 +88,8 @@
                     <input type="hidden"
                         class="min_price max-range"
                         name="max_price"
-                           value="{{ request()->input('max_price', (int)theme_option('max_filter_price', 10) * get_current_exchange_rate()) }}"
-                           data-max="{{ (int)theme_option('max_filter_price', 10) * get_current_exchange_rate() }}"
+                           value="{{ request()->input('max_price', (int)theme_option('max_filter_price', 5) * get_current_exchange_rate()) }}"
+                           data-max="{{ (int)theme_option('max_filter_price', 5) * get_current_exchange_rate() }}"
                         data-label="{{ __('Max price') }}"/>
                     <div class="price_slider_amount">
                         <div class="label-input">
