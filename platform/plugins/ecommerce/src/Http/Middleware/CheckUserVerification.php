bbde584b86aa547c0953ab59cce3ed0eafc82241
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-12-19 10:26:31 +0200

    Update CheckUserVerification.php

--- platform/plugins/ecommerce/src/Http/Middleware/CheckUserVerification.php
+++ platform/plugins/ecommerce/src/Http/Middleware/CheckUserVerification.php
@@ -18,9 +18,9 @@ class CheckUserVerification
     {
 //        auth()->user()->markEmailAsVerified();
 //        auth()->user()->unMarkEmailAsVerified();
-        if (auth('customer')->user() && !auth('customer')->user()->email_verified_at ) {
-            return redirect('users/verify');
-        }
+        // if (auth('customer')->user() && !auth('customer')->user()->email_verified_at ) {
+        //     return redirect('users/verify');
+        // }
         return $next($request);
     }
 }
