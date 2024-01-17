e2308a2aafa38c94b679d51cb293d0fdbb6dc426
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-01-17 10:37:51 +0200

    Update CheckUserVerification.php

--- platform/plugins/ecommerce/src/Http/Middleware/CheckUserVerification.php
+++ platform/plugins/ecommerce/src/Http/Middleware/CheckUserVerification.php
@@ -17,27 +17,15 @@ class CheckUserVerification
     public function handle(Request $request, Closure $next)
     {
 
-        dd(auth('customer')->check(),auth('customer')->user()->email_verified_at,$request);
-        // Check if the user is a customer, if their email is not verified, and they are not already on the 'users/verify' page
-    if (auth('customer')->check() &&
-        !auth('customer')->user()->email_verified_at &&
-        !$request->is('users/verify')) {
-
-        // Redirect to 'users/verify' with the email as a query parameter
-        // and add an additional parameter to indicate redirection
-        return redirect('users/verify')
-                ->with('redirected_for_verification', true)
-                ->with('email', auth('customer')->user()->email);
-    }
-
-    // If the user is on the 'users/verify' page and has been redirected for verification, do not redirect again
-    if ($request->is('users/verify') && $request->session()->get('redirected_for_verification')) {
-        // Clear the redirection flag
-        $request->session()->forget('redirected_for_verification');
-        return $next($request);
-    }
-
-    // Continue with the request if the user is verified or if none of the above conditions met
-    return $next($request);
+        if (auth('customer')->user() && !auth('customer')->user()->email_verified_at ) {
+            if($request->is('users/verify')){
+                return $next($request);
+            }
+            return redirect('users/verify');
         }
+        // if (auth('customer')->user() && !auth('customer')->user()->email_verified_at ) {
+        //     return redirect('users/verify');
+        // }
+        return $next($request);
+}
 }
