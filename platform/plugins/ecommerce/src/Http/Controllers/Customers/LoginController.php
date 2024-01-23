ac0bb45dde57eb384a46ff194ecc08ce99a3bb03
Amirezalla <allahverdiamirreza@gmail.com>
2024-01-23 17:12:19 +0100

    Update LoginController.php

--- platform/plugins/ecommerce/src/Http/Controllers/Customers/LoginController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Customers/LoginController.php
@@ -80,6 +80,7 @@ class LoginController extends Controller
             Cache::forget('VERIFICATION_URL_CUSTOMER_'.$user->id);
         }else if($user->email_verified_at && $user->status == 'locked'){
             session(['message' => 'La tua verifica è stata completata. Devi attendere alcune ore perché l\'amministratore approvi la tua richiesta di registrazione!']);
+            dd($_SESSION);
             return redirect('/login');
         }else{
             return redirect('/login');
