a4295d27f1fa992d1d7a815c4ccca682730f4dd5
Amirezalla <allahverdiamirreza@gmail.com>
2024-01-23 17:17:02 +0100

    Update login.blade.php

--- platform/themes/wowy/views/ecommerce/customers/login.blade.php
+++ platform/themes/wowy/views/ecommerce/customers/login.blade.php
@@ -29,8 +29,7 @@ use Illuminate\Support\Facades\Crypt;
                 <div class="alert alert-success">
                     {{ session('message') }}
                 </div>
-            @endif
-            @dd(session()->get('message'));
+                @endif
                 <div class="login_wrap widget-taber-content p-30 background-white border-radius-10">
                     <div class="padding_eight_all bg-white">
                         <div class="heading_s1 mb-20">
