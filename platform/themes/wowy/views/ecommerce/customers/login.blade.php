8bcf9f7c6afee0f2ddc8f603b4f7e1b376fd5c6f
Amirezalla <allahverdiamirreza@gmail.com>
2024-01-23 17:03:57 +0100

    Update login.blade.php

--- platform/themes/wowy/views/ecommerce/customers/login.blade.php
+++ platform/themes/wowy/views/ecommerce/customers/login.blade.php
@@ -25,11 +25,11 @@ use Illuminate\Support\Facades\Crypt;
     <div class="container">
         <div class="row">
             <div class="col-lg-6 m-auto">
-            @if (isset($_GET['verify-message']))
-                    <div class="alert alert-info">
-                        La tua verifica è stata completata. Devi attendere alcune ore perché l'amministratore approva la tua richiesta di registrazione!
-                    </div>
-                @endif
+                @if (session('message'))
+                <div class="alert alert-success">
+                    {{ session('message') }}
+                </div>
+            @endif
                 <div class="login_wrap widget-taber-content p-30 background-white border-radius-10">
                     <div class="padding_eight_all bg-white">
                         <div class="heading_s1 mb-20">
