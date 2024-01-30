054f46bacd9738e1b9ed42d5e0afd3854ffcc41b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-01-30 20:48:14 +0200

    Update product-item.blade.php

--- platform/themes/wowy/views/ecommerce/includes/product-item.blade.php
+++ platform/themes/wowy/views/ecommerce/includes/product-item.blade.php
@@ -101,43 +101,45 @@
             <form class="add-to-cart-form" method="POST" action="{{ route('public.cart.add-to-cart') }}">
                 <div class="product-price">
                     @csrf
-                @if (isset($reserved_price))
-                    @if (!isset($offerDetail) && $reserved_price !== $product->price)
-                        <span>{{ format_price($reserved_price) }}</span>
-                        <input type="hidden" name="product_price" class="hidden-product-id"
-                            value="{{ $reserved_price }}" />
-                        <span class="old-price">{{ format_price($product->price_with_taxes) }}</span>
-                    @elseif (isset($offerDetail) &&
-                            ($offerType == 1 || $offerType == 2 || $offerType == 3) &&
-                            $offerDetail->product_price !== $product->price)
-                        <span>{{ format_price($offerDetail->product_price) }}</span>
-                        <input type="hidden" name="product_price" class="hidden-product-id"
-                            value="{{ $offerDetail->product_price ? $offerDetail->product_price : $pricelist[0]->final_price }}" />
-                        <span class="old-price">{{ format_price($product->price_with_taxes) }}</span>
-                    @elseif ($offerDetail && ($offerType != 1 || $offerType != 2 || $offerType != 3))
-                        <span>{{ format_price($reserved_price) }}</span>
-                        <input type="hidden" name="product_price" class="hidden-product-id"
-                            value="{{ $reserved_price }}" />
-                        <span class="old-price">{{ format_price($product->price_with_taxes) }}</span>
+                    <input type="hidden" name="id" class="hidden-product-id"
+                        value="{{ $product->is_variation || !$product->defaultVariation->product_id ? $product->id : $product->defaultVariation->product_id }}" />
+                    @if (isset($reserved_price))
+                        @if (!isset($offerDetail) && $reserved_price !== $product->price)
+                            <span>{{ format_price($reserved_price) }}</span>
+                            <input type="hidden" name="product_price" class="hidden-product-id"
+                                value="{{ $reserved_price }}" />
+                            <span class="old-price">{{ format_price($product->price_with_taxes) }}</span>
+                        @elseif (isset($offerDetail) &&
+                                ($offerType == 1 || $offerType == 2 || $offerType == 3) &&
+                                $offerDetail->product_price !== $product->price)
+                            <span>{{ format_price($offerDetail->product_price) }}</span>
+                            <input type="hidden" name="product_price" class="hidden-product-id"
+                                value="{{ $offerDetail->product_price ? $offerDetail->product_price : $pricelist[0]->final_price }}" />
+                            <span class="old-price">{{ format_price($product->price_with_taxes) }}</span>
+                        @elseif ($offerDetail && ($offerType != 1 || $offerType != 2 || $offerType != 3))
+                            <span>{{ format_price($reserved_price) }}</span>
+                            <input type="hidden" name="product_price" class="hidden-product-id"
+                                value="{{ $reserved_price }}" />
+                            <span class="old-price">{{ format_price($product->price_with_taxes) }}</span>
+                        @endif
+                    @else
+                        <span>{{ format_price($product->front_sale_price_with_taxes) }}</span>
                     @endif
-                @else
-                    <span>{{ format_price($product->front_sale_price_with_taxes) }}</span>
-                @endif
 
-
-            </div>
-
-            {!! apply_filters('ecommerce_after_product_price_in_listing', null, $product) !!}
-
-            @if (EcommerceHelper::isCartEnabled())
-                <div class="product-action-1 show " @if (!EcommerceHelper::isReviewEnabled()) style="bottom: 10px;" @endif>
-                    <button type="submit"
-                        class="button button-add-to-cart @if ($product->isOutOfStock()) btn-disabled @endif"
-                        type="submit" @if ($product->isOutOfStock()) disabled @endif>
-                        <i class="far fa-shopping-bag"></i></button>
 
                 </div>
-            @endif
+
+                {!! apply_filters('ecommerce_after_product_price_in_listing', null, $product) !!}
+
+                @if (EcommerceHelper::isCartEnabled())
+                    <div class="product-action-1 show " @if (!EcommerceHelper::isReviewEnabled()) style="bottom: 10px;" @endif>
+                        <button type="submit"
+                            class="button button-add-to-cart @if ($product->isOutOfStock()) btn-disabled @endif"
+                            type="submit" @if ($product->isOutOfStock()) disabled @endif>
+                            <i class="far fa-shopping-bag"></i></button>
+
+                    </div>
+                @endif
             </form>
         </div>
     </div>
