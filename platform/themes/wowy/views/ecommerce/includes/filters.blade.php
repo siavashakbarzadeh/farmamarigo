309a16a59279e4a266c127cbd77b2b3fc9d1df7b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-01-30 21:44:26 +0200

    Update filters.blade.php

--- platform/themes/wowy/views/ecommerce/includes/filters.blade.php
+++ platform/themes/wowy/views/ecommerce/includes/filters.blade.php
@@ -109,7 +109,7 @@
                                                 <input class="category-check form-check-input" name="category[]"
                                                     type="checkbox" id="brand-filter-{{ $category->id }}"
                                                     value="{{ $category->id }}"
-                                                    @if (in_array($category->id, request()->input('category', []))) checked @endif>
+                                                    @if (in_array($category->id, request()->input('categories', []))) checked @endif>
                                             </div>
                                             <div class="col-10">
                                                 <label class=" brands-check form-check-label"
@@ -126,142 +126,142 @@
                     </div>
                 </div>
             </div>
-    @endif
+        @endif
 
-    @php
-        use Botble\Ecommerce\Models\Product;
-        use Botble\Ecommerce\Models\Brand;
-
-        // Retrieve all unique brand IDs from products
-        $brandIds = Product::where('status', \Botble\Base\Enums\BaseStatusEnum::PUBLISHED)
-            ->pluck('brand_id')
-            ->unique();
-
-        // Retrieve brands that are used in products
-        $brands = Brand::whereIn('id', $brandIds)->get();
-    @endphp
-    <div class="col-12 mb-5 widget-filter-item">
-        <div class="accordion my-2" id="brandsAccordionWrapper ">
-            <strong style="color: #005ba1;font-size: 16px;font-weight: 700;display: block;"
-                class="mb-1">Produttore</strong>
-            <div class="accordion-item">
-
-                <div class="accordion-header " id="headingOne">
-                    <button class="accordion-button py-2 px-4" id="accordion-button" type="button"
-                        data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
-                        aria-controls="collapseOne" style="background-color: white;color:black;">
-                        Produttore
-                    </button>
-                </div>
-                <div id="collapseOne" class="accordion-collapse collapse " aria-labelledby="headingOne">
-                    <div class="accordion-body brands-place">
-                        @foreach ($brands as $brand)
-                            <div class="row"
-                                style="border: 1px solid #F5F5F5;border-right: 0;border-left: 0;border-top: 0; margin-top:5px">
-                                <div class="col-1" style="align-self: center">
-                                    <input class="brands-check form-check-input" name="brands[]" type="checkbox"
-                                        id="brand-filter-{{ $brand->id }}" value="{{ $brand->id }}"
-                                        @if (in_array($brand->id, request()->input('brands', []))) checked @endif>
-                                </div>
-                                <div class="col-10">
-                                    <label class=" brands-check form-check-label"
-                                        for="brand-filter-{{ $brand->id }}"><span
-                                            class="d-inline-block">{{ $brand->name }}</span> </label>
-                                </div>
-                            </div>
-                        @endforeach
-
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-    @php
-        $tags = app(\Botble\Ecommerce\Repositories\Interfaces\ProductTagInterface::class)->advancedGet([
-            'condition' => ['status' => \Botble\Base\Enums\BaseStatusEnum::PUBLISHED],
-            'withCount' => ['products'],
-            'order_by' => ['products_count' => 'desc'],
-            'take' => 20,
-        ]);
-    @endphp
-    {{--        @if (count($tags) > 0) --}}
-    {{--            <div class="col-lg-3 col-md-4 mb-lg-0 mb-md-5 mb-sm-5 widget-filter-item"> --}}
-    {{--                <h5 class="mb-20 widget__title" data-title="{{ __('Tag') }}">{{ __('By :name', ['name' => __('tags')]) }}</h5> --}}
-    {{--                <div class="custome-checkbox"> --}}
-    {{--                    @foreach ($tags as $tag) --}}
-    {{--                        <input class="form-check-input" --}}
-    {{--                               name="tags[]" --}}
-    {{--                               type="checkbox" --}}
-    {{--                               id="tag-filter-{{ $tag->id }}" --}}
-    {{--                               value="{{ $tag->id }}" --}}
-    {{--                               @if (in_array($tag->id, request()->input('tags', []))) checked @endif> --}}
-    {{--                        <label class="form-check-label" for="tag-filter-{{ $tag->id }}"><span class="d-inline-block">{{ $tag->name }}</span> <span class="d-inline-block">({{ $tag->products_count }})</span> </label> --}}
-    {{--                        <br> --}}
-    {{--                    @endforeach --}}
-    {{--                </div> --}}
-    {{--            </div> --}}
-    {{--        @endif --}}
-
-    <div class="col-lg-12 widget-filter-item mt-10" data-type="price">
-        <h5 class="mb-5 widget__title" data-title="{{ __('Price') }}">{{ __('Price') }}</h5>
-        <div class="price-filter range">
-            <div class="price-filter-inner">
-                <div class="slider-range"></div>
-                <input type="hidden" class="min_price min-range" name="min_price"
-                    value="{{ request()->input('min_price', 0) }}" data-min="0"
-                    data-label="{{ __('Min price') }}" />
-                <input type="hidden" class="min_price max-range" name="max_price"
-                    value="{{ request()->input('max_price', (int) theme_option('max_filter_price', 5) * get_current_exchange_rate()) }}"
-                    data-max="{{ (int) theme_option('max_filter_price', 5) * get_current_exchange_rate() }}"
-                    data-label="{{ __('Max price') }}" />
-                <div class="price_slider_amount">
-                    <div class="label-input">
-                        <span class="d-inline-block"> Tra </span>
-                        <span class="from d-inline-block"></span>
-                        <span class="to d-inline-block"></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    {{--            <div class="row"> --}}
-    {{--                {!! render_product_swatches_filter([ --}}
-    {{--                    'view' => Theme::getThemeNamespace() . '::views.ecommerce.attributes.attributes-filter-renderer' --}}
-    {{--                ]) !!} --}}
-    {{--            </div> --}}
-</div>
-
-{{--    <a class="show-advanced-filters" href="#"> --}}
-{{--        <span class="title">{{ __('Advanced filters') }}</span> --}}
-{{--        <i class="far fa-angle-up angle-down"></i> --}}
-{{--        <i class="far fa-angle-down angle-up"></i> --}}
-{{--    </a> --}}
-
-{{--    <div class="advanced-search-widgets" style="display: none"> --}}
-{{--        <div class="row"> --}}
-{{--            {!! render_product_swatches_filter([ --}}
-{{--                'view' => Theme::getThemeNamespace() . '::views.ecommerce.attributes.attributes-filter-renderer' --}}
-{{--            ]) !!} --}}
-{{--        </div> --}}
-{{--    </div> --}}
-
-<div class="widget mt-3 row">
-
-    <button type="submit" class="btn btn-sm btn-default col-12"
-        style="font-size:small;border-radius:25px; text-transform:none;"><i class="fa fa-filter mr-5 ml-0"></i>
-        {{ __('Filter') }}</button>
-</div>
-
-<div class="widget mt-3 row">
-    <div class="container">
         @php
-            $category = $_GET['categories'][0] ?? null;
+            use Botble\Ecommerce\Models\Product;
+            use Botble\Ecommerce\Models\Brand;
+
+            // Retrieve all unique brand IDs from products
+            $brandIds = Product::where('status', \Botble\Base\Enums\BaseStatusEnum::PUBLISHED)
+                ->pluck('brand_id')
+                ->unique();
+
+            // Retrieve brands that are used in products
+            $brands = Brand::whereIn('id', $brandIds)->get();
         @endphp
-        <button class="btn btn-sm col-12 clear_filter dib clear_all_filter"
-            style="color:#51b448;background-color:white !important;font-size: small; border-radius: 25px; text-transform: none;"
-            href="{{ URL::current() }}">{{ __('Cancella filtri') }}</button>
+        <div class="col-12 mb-5 widget-filter-item">
+            <div class="accordion my-2" id="brandsAccordionWrapper ">
+                <strong style="color: #005ba1;font-size: 16px;font-weight: 700;display: block;"
+                    class="mb-1">Produttore</strong>
+                <div class="accordion-item">
+
+                    <div class="accordion-header " id="headingOne">
+                        <button class="accordion-button py-2 px-4" id="accordion-button" type="button"
+                            data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
+                            aria-controls="collapseOne" style="background-color: white;color:black;">
+                            Produttore
+                        </button>
+                    </div>
+                    <div id="collapseOne" class="accordion-collapse collapse " aria-labelledby="headingOne">
+                        <div class="accordion-body brands-place">
+                            @foreach ($brands as $brand)
+                                <div class="row"
+                                    style="border: 1px solid #F5F5F5;border-right: 0;border-left: 0;border-top: 0; margin-top:5px">
+                                    <div class="col-1" style="align-self: center">
+                                        <input class="brands-check form-check-input" name="brands[]" type="checkbox"
+                                            id="brand-filter-{{ $brand->id }}" value="{{ $brand->id }}"
+                                            @if (in_array($brand->id, request()->input('brands', []))) checked @endif>
+                                    </div>
+                                    <div class="col-10">
+                                        <label class=" brands-check form-check-label"
+                                            for="brand-filter-{{ $brand->id }}"><span
+                                                class="d-inline-block">{{ $brand->name }}</span> </label>
+                                    </div>
+                                </div>
+                            @endforeach
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        @php
+            $tags = app(\Botble\Ecommerce\Repositories\Interfaces\ProductTagInterface::class)->advancedGet([
+                'condition' => ['status' => \Botble\Base\Enums\BaseStatusEnum::PUBLISHED],
+                'withCount' => ['products'],
+                'order_by' => ['products_count' => 'desc'],
+                'take' => 20,
+            ]);
+        @endphp
+        {{--        @if (count($tags) > 0) --}}
+        {{--            <div class="col-lg-3 col-md-4 mb-lg-0 mb-md-5 mb-sm-5 widget-filter-item"> --}}
+        {{--                <h5 class="mb-20 widget__title" data-title="{{ __('Tag') }}">{{ __('By :name', ['name' => __('tags')]) }}</h5> --}}
+        {{--                <div class="custome-checkbox"> --}}
+        {{--                    @foreach ($tags as $tag) --}}
+        {{--                        <input class="form-check-input" --}}
+        {{--                               name="tags[]" --}}
+        {{--                               type="checkbox" --}}
+        {{--                               id="tag-filter-{{ $tag->id }}" --}}
+        {{--                               value="{{ $tag->id }}" --}}
+        {{--                               @if (in_array($tag->id, request()->input('tags', []))) checked @endif> --}}
+        {{--                        <label class="form-check-label" for="tag-filter-{{ $tag->id }}"><span class="d-inline-block">{{ $tag->name }}</span> <span class="d-inline-block">({{ $tag->products_count }})</span> </label> --}}
+        {{--                        <br> --}}
+        {{--                    @endforeach --}}
+        {{--                </div> --}}
+        {{--            </div> --}}
+        {{--        @endif --}}
+
+        <div class="col-lg-12 widget-filter-item mt-10" data-type="price">
+            <h5 class="mb-5 widget__title" data-title="{{ __('Price') }}">{{ __('Price') }}</h5>
+            <div class="price-filter range">
+                <div class="price-filter-inner">
+                    <div class="slider-range"></div>
+                    <input type="hidden" class="min_price min-range" name="min_price"
+                        value="{{ request()->input('min_price', 0) }}" data-min="0"
+                        data-label="{{ __('Min price') }}" />
+                    <input type="hidden" class="min_price max-range" name="max_price"
+                        value="{{ request()->input('max_price', (int) theme_option('max_filter_price', 5) * get_current_exchange_rate()) }}"
+                        data-max="{{ (int) theme_option('max_filter_price', 5) * get_current_exchange_rate() }}"
+                        data-label="{{ __('Max price') }}" />
+                    <div class="price_slider_amount">
+                        <div class="label-input">
+                            <span class="d-inline-block"> Tra </span>
+                            <span class="from d-inline-block"></span>
+                            <span class="to d-inline-block"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{--            <div class="row"> --}}
+        {{--                {!! render_product_swatches_filter([ --}}
+        {{--                    'view' => Theme::getThemeNamespace() . '::views.ecommerce.attributes.attributes-filter-renderer' --}}
+        {{--                ]) !!} --}}
+        {{--            </div> --}}
     </div>
-</div>
+
+    {{--    <a class="show-advanced-filters" href="#"> --}}
+    {{--        <span class="title">{{ __('Advanced filters') }}</span> --}}
+    {{--        <i class="far fa-angle-up angle-down"></i> --}}
+    {{--        <i class="far fa-angle-down angle-up"></i> --}}
+    {{--    </a> --}}
+
+    {{--    <div class="advanced-search-widgets" style="display: none"> --}}
+    {{--        <div class="row"> --}}
+    {{--            {!! render_product_swatches_filter([ --}}
+    {{--                'view' => Theme::getThemeNamespace() . '::views.ecommerce.attributes.attributes-filter-renderer' --}}
+    {{--            ]) !!} --}}
+    {{--        </div> --}}
+    {{--    </div> --}}
+
+    <div class="widget mt-3 row">
+
+        <button type="submit" class="btn btn-sm btn-default col-12"
+            style="font-size:small;border-radius:25px; text-transform:none;"><i class="fa fa-filter mr-5 ml-0"></i>
+            {{ __('Filter') }}</button>
+    </div>
+
+    <div class="widget mt-3 row">
+        <div class="container">
+            @php
+                $category = $_GET['categories'][0] ?? null;
+            @endphp
+            <button class="btn btn-sm col-12 clear_filter dib clear_all_filter"
+                style="color:#51b448;background-color:white !important;font-size: small; border-radius: 25px; text-transform: none;"
+                href="{{ URL::current() }}">{{ __('Cancella filtri') }}</button>
+        </div>
+    </div>
 </div>
