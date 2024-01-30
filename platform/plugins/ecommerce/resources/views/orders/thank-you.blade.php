4e65cf36bc59a11fa834a8df2431eeee8a9d0de6
siavashakbarzadeh1 <91117383+siavashakbarzadeh1@users.noreply.github.com>
2024-01-30 11:42:45 +0100

    Update thank-you.blade.php

--- platform/plugins/ecommerce/resources/views/orders/thank-you.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/thank-you.blade.php
@@ -12,10 +12,10 @@
                 <div class="thank-you">
                     <div class="d-block">
                         <h3 class="thank-you-sentence" style="position:relative">
-                            <i class="fa fa-check-circle" aria-hidden="true" style="font-size:5em;color:#71d782"></i>
+                            <i class="fa fa-check-circle" aria-hidden="true" style="font-size:5em;color:#56b448"></i>
                             <div style="position: absolute;top:0px;font-size:larger;left:122px">
                                 <h3>{{ __('Your order is successfully placed') }}</h3>
-                                <p>{{ __('Nel caso in cui tu ne avessi necessità, hai la possibilità di apportare modifiche al tuo ordine per i prossimi 30 minuti.') }}</p>
+{{--                                <p>{{ __('Nel caso in cui tu ne avessi necessità, hai la possibilità di apportare modifiche al tuo ordine per i prossimi 30 minuti.') }}</p>--}}
                             </div>
 
                         </h3>
@@ -26,7 +26,7 @@
 
 {{--                <a href="{{ route('public.index') }}" class="btn payment-checkout-btn"> {{ __('Continue shopping') }} </a>--}}
 
-                <a href="https://marigopharma.marigo.collaudo.biz/customer/orders" class="btn payment-checkout-btn"style="background:#005BA1"> {{ __('I tuoi ordini') }} </a>
+                <a href="https://marigopharma.marigo.collaudo.biz/customer/orders" class="btn payment-checkout-btn"style="background:#56b448"> {{ __('I tuoi ordini') }} </a>
 
             </div>
 
