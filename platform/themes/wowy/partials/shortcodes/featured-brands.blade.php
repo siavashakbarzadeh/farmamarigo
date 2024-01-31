01ee4147b579610f801b777ac439726d73df15b1
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-01-31 13:00:00 +0200

    Update featured-brands.blade.php

--- platform/themes/wowy/partials/shortcodes/featured-brands.blade.php
+++ platform/themes/wowy/partials/shortcodes/featured-brands.blade.php
@@ -4,14 +4,14 @@
 <section class="section-padding-60">
     <div class="container">
         <h3 class="section-title style-1 mb-30 wow fadeIn animated">{!! BaseHelper::clean($title) !!}</h3>
-{{--        <div id="owl-demo" class="owl-carousel owl-theme">--}}
+        {{--        <div id="owl-demo" class="owl-carousel owl-theme"> --}}
         <div class=" owl-carousel owl-theme featured-brands-carousel ">
-{{--        <div class=" owl-carousel owl-theme brands-carousel ">--}}
+            {{--        <div class=" owl-carousel owl-theme brands-carousel "> --}}
 
             @foreach ($brands as $brand)
                 <div class="col-6">
-                    <a class="displayManufacturer" href="/products?brands={{ $brand->id }}">
-                        <img class="displayManufacturerImg" src="{{ RvMedia::getImageUrl($brand->logo ) }}">
+                    <a class="displayManufacturer" href="/products?brands[]={{ $brand->id }}">
+                        <img class="displayManufacturerImg" src="{{ RvMedia::getImageUrl($brand->logo) }}">
                     </a>
                 </div>
             @endforeach
