832530fd69daac11bb340a7dcfa3eed43eb17a44
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-01-31 11:44:40 +0200

    Update address-item.blade.php

--- platform/plugins/ecommerce/resources/views/orders/partials/address-item.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/partials/address-item.blade.php
@@ -4,11 +4,11 @@
        title="{{ $address->address }}, {{ $address->city_name }}, {{ $address->state_name }}@if (EcommerceHelper::isUsingInMultipleCountries()), {{ $address->country_name }} @endif @if (EcommerceHelper::isZipCodeEnabled() && $address->zip_code), {{ $address->zip_code }} @endif">
         {{ $address->address }}, {{ $address->city_name }}, {{ $address->state_name }}@if (EcommerceHelper::isUsingInMultipleCountries()), {{ $address->country_name }} @endif @if (EcommerceHelper::isZipCodeEnabled() && $address->zip_code), {{ $address->zip_code }} @endif
     </p>
-    <p class="phone">{{ __('Phone') }}: {{ $address->phone }}</p>
+    {{-- <p class="phone">{{ __('Phone') }}: {{ $address->phone }}</p> --}}
     @if ($address->email)
         <p class="email">{{ __('Email') }}: {{ $address->email }}</p>
     @endif
-    @if ($address->is_default)
+    {{-- @if ($address->is_default)
         <span class="default">{{ __('Default') }}</span>
-    @endif
+    @endif --}}
 </div>
