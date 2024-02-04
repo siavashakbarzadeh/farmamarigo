d0359ab8f7b2d2692b39a9876ba1d6aabae88860
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-04 15:50:57 +0200

    Update product-items.blade.php

--- platform/themes/wowy/views/ecommerce/includes/product-items.blade.php
+++ platform/themes/wowy/views/ecommerce/includes/product-items.blade.php
@@ -6,7 +6,7 @@
         $layout = $requestLayout;
     }
 
-    $layout = ($layout && in_array($layout, array_keys(get_product_single_layouts()))) ? $layout : 'product-full-width';
+    $layout = $layout && in_array($layout, array_keys(get_product_single_layouts())) ? $layout : 'product-full-width';
 @endphp
 
 <div class="list-content-loading">
@@ -18,7 +18,9 @@
 
 <div class="shop-product-filter">
     <div class="totall-product">
-        <p>{!! BaseHelper::clean(__('We found :total items for you!', ['total' => '<strong class="text-brand">' . $products->total() . '</strong>'])) !!}</p>
+        <p>{!! BaseHelper::clean(
+            __('We found :total items for you!', ['total' => '<strong class="text-brand">' . $products->total() . '</strong>']),
+        ) !!}</p>
     </div>
     @include(Theme::getThemeNamespace() . '::views/ecommerce/includes/sort')
 </div>
@@ -31,11 +33,25 @@
 <div class="row">
     @forelse ($products as $product)
         <div class="col-lg-{{ 12 / ($layout != 'product-full-width' ? 3 : 4) }} col-md-4">
-            @include(Theme::getThemeNamespace() . '::views.ecommerce.includes.product-item', compact('product'))
+            @include(Theme::getThemeNamespace() . '::views.ecommerce.includes.product-item',
+                compact('product'))
         </div>
     @empty
-        <div class="mt__60 mb__60 text-center">
-            <p>{{ __('No products found!') }}</p>
+        <div class="container">
+            <div class="row mt-4">
+                <div class="col-1" style="text-align-last: end">
+                    <i class="fa fa-exclamation" style="font-size: xxx-large; color:#e97979"></i>
+                </div>
+                <div class="col-11">
+                    <h3>
+                        Nessun prodotto trovato secondo i criteri di ricerca impostati
+                    </h3>
+                    <h4>
+                        Cambiare combinazione di filtri e riprovare.
+                    </h4>
+
+                </div>
+            </div>
         </div>
     @endforelse
 </div>
