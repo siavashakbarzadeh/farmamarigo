2c86ca1b0e09bf4c04e9c32e72b9563e9c323746
Amirezalla <allahverdiamirreza@gmail.com>
2024-02-05 11:52:51 +0100

    Update OrderPaymentFailed.php

--- platform/plugins/ecommerce/src/Mail/OrderPaymentFailed.php
+++ platform/plugins/ecommerce/src/Mail/OrderPaymentFailed.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OrderConfirmed extends Mailable
+class OrderPaymentFailed extends Mailable
 {
     use Queueable, SerializesModels;
 
