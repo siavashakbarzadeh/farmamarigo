989e97f56b2b034fcfc4455363846340697e1a8f
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-05 21:53:26 +0200

    Update list.blade.php

--- platform/themes/wowy/views/ecommerce/customers/orders/list.blade.php
+++ platform/themes/wowy/views/ecommerce/customers/orders/list.blade.php
@@ -17,14 +17,15 @@
                                 <div class="container">
                                     <div class="row">
                                         <div class="col-1"></div>
-                                        <div class="col-10 row" style="display: flex;
+                                        <div class="col-10 row"
+                                            style="display: flex;
                                         flex-basis: content;margin-left: 28px;
                                         font-size: smaller;">
-{{--                                            <div class="col">Modifica</div>--}}
+                                            {{--                                            <div class="col">Modifica</div> --}}
                                             <div class="col">Stampa</div>
                                             <div class="col">Rivedi</div>
                                             <div class="col">Riordina</div>
-                                            {{--<div class="col">Riordina</div>--}}
+                                            {{-- <div class="col">Riordina</div> --}}
                                         </div>
                                     </div>
                                 </div>
@@ -35,19 +36,21 @@
                         @forelse ($orders as $order)
                             <tr>
                                 <td>{{ get_order_code($order->id) }}</td>
-{{--                                <td>{{ $order->created_at->format('Y/m/d h:m') }}</td>--}}
-                                <td>{{ \Carbon\Carbon::parse($order->created_at)}}</td>
-                                <td >{{ __(':price for :total item(s)', ['price' => $order->amount_format, 'total' => $order->products_count]) }}</td>
-{{--                                <td>{{ 'price' => $order->amount_format 'total' => $order->products_count }}</td>--}}
+                                {{--                                <td>{{ $order->created_at->format('Y/m/d h:m') }}</td> --}}
+                                <td>{{ \Carbon\Carbon::parse($order->created_at) }}</td>
+                                <td>{{ __(':price for :total item(s)', ['price' => $order->amount_format, 'total' => $order->products_count]) }}
+                                </td>
+                                {{--                                <td>{{ 'price' => $order->amount_format 'total' => $order->products_count }}</td> --}}
                                 <td style="text-align:center">
-                                    @if( $order->is_finished && $order->is_confirmed)
-                                       <label class="btn-success p-1 rounded small" >Completato</label>
+                                    @if ($order->is_finished && $order->is_confirmed)
+                                        <label class="btn-success p-1 rounded small">Completato</label>
                                     @endif
-                                    @if( $order->is_finished && !$order->is_confirmed)
-                                       <label class="btn-success p-1 rounded small" style="background-color:#f9844a">Mancato Pagamento</label>
+                                    @if ($order->is_finished && !$order->is_confirmed)
+                                        <label class="btn-success p-1 rounded small"
+                                            style="background-color:#f9844a">Mancato Pagamento</label>
                                     @endif
-                                    @if( $order->status=='canceled')
-                                       <label class="btn-danger p-1 rounded small">Annullato</label>
+                                    @if ($order->status == 'canceled')
+                                        <label class="btn-danger p-1 rounded small">Annullato</label>
                                     @endif
                                 </td>
                                 <td style="display: flex;flex-direction: row;justify-content: center;align-items: center;">
@@ -56,14 +59,16 @@
 
                                     <div class="row list-order-action">
                                         <div class="col-3">
-                                            @if($order->payment_id!==NULL && $order->isInvoiceAvailable())
-                                            <a class='btn btn-primary btn-sm' href="{{ route('customer.print-order', $order->id) }}"
-                                               style="color:white !important;width:40px;height:40px;border-radius: 50%;text-align: center;display: flex;flex-direction: row;justify-content: center;align-items: center;">
-                                               <i class="fa fa-print"></i>
-                                            </a>
+                                            @if ($order->payment_id !== null && $order->isInvoiceAvailable())
+                                                <a class='btn btn-primary btn-sm'
+                                                    href="{{ route('customer.print-order', $order->id) }}"
+                                                    style="color:white !important;width:40px;height:40px;border-radius: 50%;text-align: center;display: flex;flex-direction: row;justify-content: center;align-items: center;">
+                                                    <i class="fa fa-print"></i>
+                                                </a>
                                             @endif
-                                            @if($order->payment_id==NULL)
-                                                <a class='btn btn-primary btn-sm' href="/retry-checkout?orderToken={{ $order->token }}"
+                                            @if ($order->payment_id == null)
+                                                <a class='btn btn-primary btn-sm'
+                                                    href="/checkout/{{ $order->token }}/recover"
                                                     style="background-color:#f9844a;color:white !important;width:40px;height:40px;border-radius: 50%;text-align: center;display: flex;flex-direction: row;justify-content: center;align-items: center;">
                                                     <i class="fa fa-credit-card"></i>
                                                 </a>
@@ -71,11 +76,17 @@
 
                                         </div>
                                         <div class="col-3">
-                                            <a class="btn btn-info btn-sm"  style="color:white !important;width:40px;height:40px;border-radius: 50%;text-align: center;display: flex;flex-direction: row;justify-content: center;align-items: center;" href="{{ route('customer.orders.view', $order->id) }}"><i class="fa fa-eye"></i></a>
+                                            <a class="btn btn-info btn-sm"
+                                                style="color:white !important;width:40px;height:40px;border-radius: 50%;text-align: center;display: flex;flex-direction: row;justify-content: center;align-items: center;"
+                                                href="{{ route('customer.orders.view', $order->id) }}"><i
+                                                    class="fa fa-eye"></i></a>
                                         </div>
-                                        <form action="{{ route('customer.orders.reorder',$order->id) }}" method="post" class="col-3">
+                                        <form action="{{ route('customer.orders.reorder', $order->id) }}" method="post"
+                                            class="col-3">
                                             @csrf
-                                            <button type="submit" class="btn btn-info btn-sm" style="width:40px;height:40px;border-radius: 50%;text-align: center;display: flex;flex-direction: row;justify-content: center;align-items: center;"><i class="fa fa-repeat"></i></button>
+                                            <button type="submit" class="btn btn-info btn-sm"
+                                                style="width:40px;height:40px;border-radius: 50%;text-align: center;display: flex;flex-direction: row;justify-content: center;align-items: center;"><i
+                                                    class="fa fa-repeat"></i></button>
                                         </form>
                                     </div>
 
