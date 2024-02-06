8bf6f66e64bbda4a7c0f33915975c61f7e78bf8f
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-06 11:21:07 +0200

    Update list.blade.php

--- platform/themes/wowy/views/ecommerce/customers/orders/list.blade.php
+++ platform/themes/wowy/views/ecommerce/customers/orders/list.blade.php
@@ -70,7 +70,8 @@
                                                 </a>
                                             @endif
                                             @php
-                                                $payment = Payment::where('order_id', $order->id)->get();
+                                                $payment = Payment::where('order_id', $order->id)->first();
+                                                dd($payment);
                                             @endphp
                                             @if ($payment->payment_channel == 'paypal' && ($payment->status = 'pending'))
                                                 <a class='btn btn-primary btn-sm'
