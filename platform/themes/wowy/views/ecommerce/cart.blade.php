f16669561e40b2ad8ab29a176e53d67a9e7226f7
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-08 13:45:06 +0200

    Update cart.blade.php

--- platform/themes/wowy/views/ecommerce/cart.blade.php
+++ platform/themes/wowy/views/ecommerce/cart.blade.php
@@ -65,11 +65,8 @@
                                                     } else {
                                                         $product_id = $cartItem->id;
                                                     }
-                                                    dd($product_id);
                                                     $product = Product::find($product_id)->first();
-
                                                     $pricelist = DB::connection('mysql')->select("select * from ec_pricelist where product_id=$product_id and customer_id=$userid");
-
                                                     if ($pricelist) {
                                                         $offerDetail = OffersDetail::where('product_id', $product_id)->where('customer_id', $userid)->first();
                                                         if ($offerDetail) {
