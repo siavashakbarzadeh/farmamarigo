42da44880a10abecec5f8262eb598121b516f0d7
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-09 14:28:14 +0200

    Update PublicCartController.php

--- platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCartController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCartController.php
@@ -350,6 +350,7 @@ class PublicCartController extends Controller
                 if ($offer && $offer->type == 4 && $cartItem->qty >= 3) {
                     // Apply discount for offer type 4 if quantity is 3 or more
                     $discountedPrice = $pricelist[0]->final_price * floor($cartItem->qty / 3);
+                    dd('here');
                     return $discountedPrice;
                 }else{
                     return 0;
