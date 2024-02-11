53292781196099b343b93b53c9e3e7756c57b348
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-11 14:34:04 +0200

    Update PublicCheckoutController.php

--- platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCheckoutController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Fronts/PublicCheckoutController.php
@@ -1105,11 +1105,11 @@ class PublicCheckoutController
     $order = $this->orderRepository->findOrFail($request->orderId);
 
     $RealOrder=Order::where('token',$order->token)->where('shipping_option',NULL)->first();
-    if($RealOrder){
+
         $products=$RealOrder->products;
         $this->addProductToOrder($order, $products->toArray());
         $RealOrder->delete();
-    }
+    
 
     $arguments=[
         'account_id' => auth('customer')->user()->id,
