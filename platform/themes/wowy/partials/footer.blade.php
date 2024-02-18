0309ab4a551563ea33fcfa286e1d43ced781d2c2
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-19 00:20:03 +0200

    Update footer.blade.php

--- platform/themes/wowy/partials/footer.blade.php
+++ platform/themes/wowy/partials/footer.blade.php
@@ -548,11 +548,13 @@
         // For categories
         const categories = urlParams.getAll('categories[]');
         categories.forEach(function(categoryId) {
+            console.log('category', categoryId)
             $(`#category-filter-${categoryId}`).attr('checked', true);
         });
         const brands = urlParams.getAll('brands[]');
         brands.forEach(function(brandId) {
             $(`#brand-filter-${brandId}`).attr('checked', true);
+            console.log('brand', brandId)
         });
     }
 
