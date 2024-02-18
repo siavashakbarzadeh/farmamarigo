a4ac872be3e35025df12212c45720c28f129a91e
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-18 21:49:48 +0200

    Update FilterController.php

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -32,36 +32,41 @@ class FilterController extends BaseController
 
         // Update brands based on selected categories if any category is selected
         // If no categories are selected, get all brands
-        if (!empty($categoryIds)) {
-            $brands = Brand::whereHas('products', function ($query) use ($categoryIds) {
-                $query->whereExists(function ($subQuery) use ($categoryIds) {
-                    $subQuery->select(DB::raw(1))
-                              ->from('ec_product_categories')
-                              ->join('ec_product_category_product', 'ec_product_categories.id', '=', 'ec_product_category_product.category_id')
-                              ->whereRaw('ec_products.id = ec_product_category_product.product_id')
-                              ->whereIn('ec_product_categories.id', $categoryIds) // Specify the table name
-                              ->where('status', "published");
-                });
-            })->get();
-        } else {
-            $brands = Brand::whereIn('id',$brandIds);
-        }
-
-        // Similarly, update categories based on selected brands if any brand is selected
-        // If no brands are selected, get all categories
-        if (!empty($brandIds)) {
-            $categories = ProductCategory::whereHas('products', function ($query) use ($brandIds) {
-                $query->whereIn('brand_id', $brandIds)
-                      ->where('status', "published");
-            })->get();
-        } else {
+        if($request->type=='category'){
+            if (!empty($categoryIds)) {
+                $brands = Brand::whereHas('products', function ($query) use ($categoryIds) {
+                    $query->whereExists(function ($subQuery) use ($categoryIds) {
+                        $subQuery->select(DB::raw(1))
+                                  ->from('ec_product_categories')
+                                  ->join('ec_product_category_product', 'ec_product_categories.id', '=', 'ec_product_category_product.category_id')
+                                  ->whereRaw('ec_products.id = ec_product_category_product.product_id')
+                                  ->whereIn('ec_product_categories.id', $categoryIds) // Specify the table name
+                                  ->where('status', "published");
+                    });
+                })->get();
+            } else {
+                $brands = Brand::whereIn('id',$brandIds);
+            }
             $categories = ProductCategory::whereIn('id',$categoryIds);
+            return response()->json([
+                'html' => view('theme.wowy::views.ecommerce.includes.filters', compact('brands','categories'))->render(),
+            ]);
+        }else{
+            if (!empty($brandIds)) {
+                $categories = ProductCategory::whereHas('products', function ($query) use ($brandIds) {
+                    $query->whereIn('brand_id', $brandIds)
+                          ->where('status', "published");
+                })->get();
+            } else {
+                $categories = ProductCategory::whereIn('id',$categoryIds);
+            }
+            $brands = Brand::whereIn('id',$brandIds);
+            return response()->json([
+                'html' => view('theme.wowy::views.ecommerce.includes.filters', compact('brands','categories'))->render(),
+            ]);
         }
-
-        // Compact both brands and categories into the response
-        return response()->json([
-            'html' => view('theme.wowy::views.ecommerce.includes.filters', compact('brands','categories'))->render(),
-        ]);
+    
+        
     }
 
 }
