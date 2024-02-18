4b17543e901704ee2186ed56c0d77a370e44a491
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-19 01:33:20 +0200

    Update extras.blade.php

--- platform/plugins/ecommerce/resources/views/products/partials/extras.blade.php
+++ platform/plugins/ecommerce/resources/views/products/partials/extras.blade.php
@@ -3,63 +3,68 @@
         <h4><span>{{ trans('plugins/ecommerce::products.related_products') }}</span></h4>
     </div>
     <div class="widget-body">
-        <div class="form-group mb-3">
-            <label class="control-label">{{ trans('plugins/ecommerce::products.related_products') }}</label>
-            <input type="hidden" name="related_products" value="@if ($product) {{ implode(',', $product->products()->allRelatedIds()->toArray()) }} @endif" />
-            <div class="box-search-advance product">
-                <div>
-                    <input type="text" class="next-input textbox-advancesearch" placeholder="{{ trans('plugins/ecommerce::products.search_products') }}" data-target="{{ $dataUrl }}">
-                </div>
-                <div class="panel panel-default">
 
-                </div>
-            </div>
-            @include('plugins/ecommerce::products.partials.selected-products-list', ['products' => $product ? $product->products : collect(), 'includeVariation' => false])
-        </div>
-        <hr>
         <div class="form-group mb-3">
             <label class="control-label">{{ trans('plugins/ecommerce::products.cross_selling_products') }}</label>
-            <input type="hidden" name="cross_sale_products" value="@if ($product) {{ implode(',', $product->crossSales()->allRelatedIds()->toArray()) }} @endif"/>
+            <input type="hidden" name="cross_sale_products"
+                value="@if ($product) {{ implode(',', $product->crossSales()->allRelatedIds()->toArray()) }} @endif" />
             <div class="box-search-advance product">
                 <div>
-                    <input type="text" class="next-input textbox-advancesearch" placeholder="{{ trans('plugins/ecommerce::products.search_products') }}" data-target="{{ $dataUrl }}">
+                    <input type="text" class="next-input textbox-advancesearch"
+                        placeholder="{{ trans('plugins/ecommerce::products.search_products') }}"
+                        data-target="{{ $dataUrl }}">
                 </div>
                 <div class="panel panel-default">
 
                 </div>
             </div>
-            @include('plugins/ecommerce::products.partials.selected-products-list', ['products' => $product ? $product->crossSales : collect(), 'includeVariation' => false])
+            @include('plugins/ecommerce::products.partials.selected-products-list', [
+                'products' => $product ? $product->crossSales : collect(),
+                'includeVariation' => false,
+            ])
         </div>
 
         @if (false)
             <hr>
             <div class="form-group mb-3">
                 <label class="control-label">{{ trans('plugins/ecommerce::products.up_selling_products') }}</label>
-                <input type="hidden" name="up_sale_products" value="@if ($product) {{ implode(',', $product->upSales()->allRelatedIds()->toArray()) }} @endif"/>
+                <input type="hidden" name="up_sale_products"
+                    value="@if ($product) {{ implode(',', $product->upSales()->allRelatedIds()->toArray()) }} @endif" />
                 <div class="box-search-advance product">
                     <div>
-                        <input type="text" class="next-input textbox-advancesearch" placeholder="{{ trans('plugins/ecommerce::products.search_products') }}" data-target="{{ $dataUrl }}">
+                        <input type="text" class="next-input textbox-advancesearch"
+                            placeholder="{{ trans('plugins/ecommerce::products.search_products') }}"
+                            data-target="{{ $dataUrl }}">
                     </div>
                     <div class="panel panel-default">
 
                     </div>
                 </div>
-                @include('plugins/ecommerce::products.partials.selected-products-list', ['products' => $product ? $product->upSales : collect(), 'includeVariation' => false])
+                @include('plugins/ecommerce::products.partials.selected-products-list', [
+                    'products' => $product ? $product->upSales : collect(),
+                    'includeVariation' => false,
+                ])
             </div>
 
             <hr>
             <div class="form-group mb-3">
                 <label class="control-label">{{ trans('plugins/ecommerce::products.grouped_products') }}</label>
-                <input type="hidden" name="grouped_products" value="@if ($product) {{ implode(',', $product->groupedItems()->pluck('product_id')->all()) }} @endif"/>
+                <input type="hidden" name="grouped_products"
+                    value="@if ($product) {{ implode(',', $product->groupedItems()->pluck('product_id')->all()) }} @endif" />
                 <div class="box-search-advance product">
                     <div>
-                        <input type="text" class="next-input textbox-advancesearch" placeholder="{{ trans('plugins/ecommerce::products.search_products') }}" data-target="{{ $dataUrl }}">
+                        <input type="text" class="next-input textbox-advancesearch"
+                            placeholder="{{ trans('plugins/ecommerce::products.search_products') }}"
+                            data-target="{{ $dataUrl }}">
                     </div>
                     <div class="panel panel-default">
 
                     </div>
                 </div>
-                @include('plugins/ecommerce::products.partials.selected-products-list', ['products' => $product ? $product->groupedProduct : collect(), 'includeVariation' => false])
+                @include('plugins/ecommerce::products.partials.selected-products-list', [
+                    'products' => $product ? $product->groupedProduct : collect(),
+                    'includeVariation' => false,
+                ])
             </div>
         @endif
     </div>
