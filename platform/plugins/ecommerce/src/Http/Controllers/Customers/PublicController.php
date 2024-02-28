5ec9b727e0125487982e65d0006badfb5f284d3b
Amirezalla <allahverdiamirreza@gmail.com>
2024-02-28 18:49:20 +0100

    Update PublicController.php

--- platform/plugins/ecommerce/src/Http/Controllers/Customers/PublicController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Customers/PublicController.php
@@ -276,7 +276,6 @@ class PublicController extends Controller
 
                         // Add to cart only if price is determined
                         if ($price !== null) {
-                            dd('price is not null');
                                 $taxRate=Tax::find(Product::find($item->product_id)->tax_id)->percentage;
                                 Cart::instance('cart')->add(
                                     $item->product_id,
