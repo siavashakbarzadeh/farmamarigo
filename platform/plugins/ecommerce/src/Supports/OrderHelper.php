0b0d45f339baa8855620d67eadd07fa9c7b5d45e
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-02-28 11:26:44 +0200

    Update OrderHelper.php

--- platform/plugins/ecommerce/src/Supports/OrderHelper.php
+++ platform/plugins/ecommerce/src/Supports/OrderHelper.php
@@ -384,6 +384,12 @@ class OrderHelper
         if ($request->input('options')) {
             $options = $this->getProductOptionData($request->input('options'));
         }
+        if($request->input('price'))
+        {
+
+            $pricelist=$request->input('price');
+
+        }
 
         /**
          * Add cart to session
@@ -406,7 +412,7 @@ class OrderHelper
                 $product->id,
                 BaseHelper::clean($parentProduct->name ?: $product->name),
                 $request->input('qty', 1),
-                $product->original_price,
+                $pricelist?$pricelist:$product->original_price,
                 [
                     'image' => RvMedia::getImageUrl($image, 'thumb', false, RvMedia::getDefaultImage()),
                     'attributes' => $product->is_variation ? $product->variation_attributes : '',
