7a32a1a070d01d068f903142543addc7d130488b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-01 12:59:49 +0200

    Update shipment-form.blade.php

--- platform/plugins/ecommerce/resources/views/orders/shipment-form.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/shipment-form.blade.php
@@ -5,12 +5,15 @@
                 <div class="form-group mb-3">
                     <label class="text-title-field">{{ trans('plugins/ecommerce::shipping.warehouse') }}:</label>
                     <div class="ui-select-wrapper">
-                        {!!
-                        Form::select('store_id', $storeLocators->pluck('name', 'id')->all(), ($storeLocators->where('is_primary', true)->first() ? $storeLocators->where('is_primary', true)->first()->id : null), [
-                            'class' => 'ui-select',
-                            'id'    => 'store_id',
-                        ])
-                    !!}
+                        {!! Form::select(
+                            'store_id',
+                            $storeLocators->pluck('name', 'id')->all(),
+                            $storeLocators->where('is_primary', true)->first() ? $storeLocators->where('is_primary', true)->first()->id : null,
+                            [
+                                'class' => 'ui-select',
+                                'id' => 'store_id',
+                            ],
+                        ) !!}
                         <svg class="svg-next-icon svg-next-icon-size-16">
                             <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#select-chevron"></use>
                         </svg>
@@ -20,8 +23,10 @@
             <div class="flexbox-grid-form-item max-width-220-px">
                 <div class="form-group mb-3">
                     <div class="form-group mb-3">
-                        <label class="text-title-field">{{ trans('plugins/ecommerce::shipping.weight_unit', ['unit' => ecommerce_weight_unit()]) }}:</label>
-                        <input type="text" class="next-input input-mask-number shipment-form-weight" name="weight" value="{{ $weight }}">
+                        <label
+                            class="text-title-field">{{ trans('plugins/ecommerce::shipping.weight_unit', ['unit' => ecommerce_weight_unit()]) }}:</label>
+                        <input type="text" class="next-input input-mask-number shipment-form-weight" name="weight"
+                            value="{{ $weight }}">
                     </div>
                 </div>
             </div>
@@ -29,7 +34,9 @@
 
         <div class="address-header p-b5">
             <div class="left">{{ trans('plugins/ecommerce::shipping.shipping_address') }}:</div>
-            <div class="right"><a href="#" class="hover-tooltip hover-underline color-blue btn-trigger-update-shipping-address">{{ trans('plugins/ecommerce::shipping.edit') }}</a></div>
+            <div class="right"><a href="#"
+                    class="hover-tooltip hover-underline color-blue btn-trigger-update-shipping-address">{{ trans('plugins/ecommerce::shipping.edit') }}</a>
+            </div>
         </div>
         <div style="clear:both;"></div>
         <div class="flexbox-grid-form flexbox-grid-form-no-outside-padding rps-form-767 mb10">
@@ -46,9 +53,14 @@
                     <div class="user-control-combox-v3" id="select-shipping-provider">
                         <div class="flexbox-grid-default" data-bs-toggle="dropdown" aria-expanded="false">
                             <div class="flexbox-content p-none-important">
-                                <input type="hidden" name="method" value="{{ $order->shipping_method }}" class="input-hidden-shipping-method">
-                                <input type="hidden" name="option" value="{{ $order->shipping_option }}" class="input-hidden-shipping-option">
-                                <input type="text" readonly="readonly" placeholder="{{ trans('plugins/ecommerce::shipping.select_shipping_method') }}" class="next-input input-dropdown input-show-shipping-method" value="{{ $order->shipping_method_name }}">
+                                <input type="hidden" name="method" value="default"
+                                    class="input-hidden-shipping-method">
+                                <input type="hidden" name="option" value="3"
+                                    class="input-hidden-shipping-option">
+                                <input type="text" readonly="readonly"
+                                    placeholder="{{ trans('plugins/ecommerce::shipping.select_shipping_method') }}"
+                                    class="next-input input-dropdown input-show-shipping-method"
+                                    value="{{ $order->shipping_method_name }}">
                             </div>
                         </div>
                         <div class="dropdown-menu dropdown-carrier font-size-13px animate-scale-dropdown bg-white">
@@ -56,26 +68,35 @@
                             <div>
                                 <table class="table table-fix-header2 table-shipping-select-options">
                                     <thead>
-                                    <tr>
-                                        <th><b>{{ trans('plugins/ecommerce::shipping.packages') }}</b><i style="display:block;font-size: xx-small; color: darkgoldenrod"><span class="ws-nm">{{ trans('plugins/ecommerce::shipping.warehouse') }} {{ get_ecommerce_setting('store_city') }}, {{ get_ecommerce_setting('store_state') }}</span></i></th>
-                                        <th class="text-end"><b>{{ trans('plugins/ecommerce::shipping.shipping_fee_cod') }}</b><i style="display:block;font-size: xx-small;">{{ trans('plugins/ecommerce::shipping.fee') }}</i></th>
-                                    </tr>
+                                        <tr>
+                                            <th><b>{{ trans('plugins/ecommerce::shipping.packages') }}</b><i
+                                                    style="display:block;font-size: xx-small; color: darkgoldenrod"><span
+                                                        class="ws-nm">{{ trans('plugins/ecommerce::shipping.warehouse') }}
+                                                        {{ get_ecommerce_setting('store_city') }},
+                                                        {{ get_ecommerce_setting('store_state') }}</span></i></th>
+                                            <th class="text-end">
+                                                <b>{{ trans('plugins/ecommerce::shipping.shipping_fee_cod') }}</b><i
+                                                    style="display:block;font-size: xx-small;">{{ trans('plugins/ecommerce::shipping.fee') }}</i>
+                                            </th>
+                                        </tr>
                                     </thead>
                                     <tbody>
-                                    @foreach($shipping as $shippingKey => $shippingItem)
-                                        @foreach($shippingItem as $subShippingKey => $subShippingItem)
-                                            <tr class="clickable-row" data-key="{{ $shippingKey }}" data-option="{{ $subShippingKey }}">
-                                                <td class="border-none pl25">
-                                                    <span class="clearfix">
-                                                        <span class="ws-nm color--green border-none-b p-none-b">{{ $subShippingItem['name'] }}</span>
-                                                    </span>
-                                                </td>
-                                                <td class="border-none text-end">
-                                                    <span>{{ format_price($subShippingItem['price']) }}</span>
-                                                </td>
-                                            </tr>
+                                        @foreach ($shipping as $shippingKey => $shippingItem)
+                                            @foreach ($shippingItem as $subShippingKey => $subShippingItem)
+                                                <tr class="clickable-row" data-key="{{ $shippingKey }}"
+                                                    data-option="{{ $subShippingKey }}">
+                                                    <td class="border-none pl25">
+                                                        <span class="clearfix">
+                                                            <span
+                                                                class="ws-nm color--green border-none-b p-none-b">{{ $subShippingItem['name'] }}</span>
+                                                        </span>
+                                                    </td>
+                                                    <td class="border-none text-end">
+                                                        <span>{{ format_price($subShippingItem['price']) }}</span>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
                                         @endforeach
-                                    @endforeach
                                     </tbody>
                                 </table>
                             </div>
@@ -83,14 +104,23 @@
                     </div>
                 </div>
             </div>
-            <div class="flexbox-grid-form flexbox-grid-form-item flexbox-grid-form-no-outside-padding rps-no-pd-none-l ps-relative max-width-220-px">
+            <div
+                class="flexbox-grid-form flexbox-grid-form-item flexbox-grid-form-no-outside-padding rps-no-pd-none-l ps-relative max-width-220-px">
                 <div class="flexbox-grid-form-item">
-                    @if ($order->payment->payment_channel == \Botble\Payment\Enums\PaymentMethodEnum::COD && $order->payment->status !== \Botble\Payment\Enums\PaymentStatusEnum::COMPLETED)
+                    @if (
+                        $order->payment->payment_channel == \Botble\Payment\Enums\PaymentMethodEnum::COD &&
+                            $order->payment->status !== \Botble\Payment\Enums\PaymentStatusEnum::COMPLETED)
                         <div class="form-group mb-3">
-                            <label class="text-title-field">{{ trans('plugins/ecommerce::shipping.cod_amount') }}:</label>
+                            <label
+                                class="text-title-field">{{ trans('plugins/ecommerce::shipping.cod_amount') }}:</label>
                             <div class="next-input--stylized">
-                                <span class="next-input-add-on next-input__add-on--before">{{ get_application_currency()->symbol }}</span>
-                                <input class="next-input next-input--invisible input-mask-number" data-thousands-separator="{{ EcommerceHelper::getThousandSeparatorForInputMask() }}" data-decimal-separator="{{ EcommerceHelper::getDecimalSeparatorForInputMask() }}" name="cod_amount" type="text" value="{{ format_price($order->amount, null, true) }}">
+                                <span
+                                    class="next-input-add-on next-input__add-on--before">{{ get_application_currency()->symbol }}</span>
+                                <input class="next-input next-input--invisible input-mask-number"
+                                    data-thousands-separator="{{ EcommerceHelper::getThousandSeparatorForInputMask() }}"
+                                    data-decimal-separator="{{ EcommerceHelper::getDecimalSeparatorForInputMask() }}"
+                                    name="cod_amount" type="text"
+                                    value="{{ format_price($order->amount, null, true) }}">
                             </div>
                         </div>
                     @endif
@@ -116,12 +146,15 @@
 
                     <input type="checkbox" checked name="send_mail" value="1">
 
-                    <span class="pre-line">{{ trans('plugins/ecommerce::shipping.send_confirmation_email_to_customer') }}</span>
+                    <span
+                        class="pre-line">{{ trans('plugins/ecommerce::shipping.send_confirmation_email_to_customer') }}</span>
                 </label>
             </div>
             <div class="flexbox-auto-content">
-                <button type="button" class="btn btn-secondary btn-close-shipment-panel">{{ trans('plugins/ecommerce::shipping.cancel') }}</button>
-                <button type="button" class="btn btn-primary btn-create-shipment">{{trans('plugins/ecommerce::shipping.finish') }}</button>
+                <button type="button"
+                    class="btn btn-secondary btn-close-shipment-panel">{{ trans('plugins/ecommerce::shipping.cancel') }}</button>
+                <button type="button"
+                    class="btn btn-primary btn-create-shipment">{{ trans('plugins/ecommerce::shipping.finish') }}</button>
             </div>
         </div>
     </div>
