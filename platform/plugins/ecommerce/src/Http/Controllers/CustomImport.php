a3e36fb254089443b6a8099c8e7417761e55b645
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-05 12:53:42 +0200

    Update CustomImport.php

--- platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
+++ platform/plugins/ecommerce/src/Http/Controllers/CustomImport.php
@@ -427,7 +427,7 @@ class CustomImport extends BaseController
                 }
             });
             
-            dd($variants);
+            dd($variants['REVIVAL']);
 
             
             // ->groupBy(function ($item) use ($variant_keys) {
